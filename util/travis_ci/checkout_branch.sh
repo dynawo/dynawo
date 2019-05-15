70eeb83fd533d474f800185b912808d9880e8f09
Gautier Bureau <gautier.bureau@gmail.com>
2019-05-15 14:29:40 +0200

    #250 Remove --depth option in git clone for Travis to avoid warnings in Sonar.

--- util/travis_ci/checkout_branch.sh
+++ util/travis_ci/checkout_branch.sh
@@ -2,10 +2,10 @@
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
   BRANCH=$TRAVIS_BRANCH;
-  GIT_COMMAND="git clone --depth=50 --branch=$BRANCH https://github.com/dynawo/dynawo.git dynawo"
+  GIT_COMMAND="git clone --branch=$BRANCH https://github.com/dynawo/dynawo.git dynawo"
   echo -n $GIT_COMMAND
 else
   PULL_REQUEST_NUM=$TRAVIS_PULL_REQUEST;
-  GIT_COMMAND="git clone --depth=50 https://github.com/dynawo/dynawo.git dynawo;cd dynawo;git fetch origin +refs/pull/$PULL_REQUEST_NUM/merge:;git checkout -qf FETCH_HEAD;"
+  GIT_COMMAND="git clone https://github.com/dynawo/dynawo.git dynawo;cd dynawo;git fetch origin +refs/pull/$PULL_REQUEST_NUM/merge:;git checkout -qf FETCH_HEAD;"
   echo -n $GIT_COMMAND
 fi
