d337ac878110efa832404d7ace03bec7dd8d7bad
Gautier Bureau <gautier.bureau@gmail.com>
2019-06-14 14:46:14 +0200

    #303 Removed --depth option for checkout of OMC because old git version does not handle it well.

--- dynawo/3rdParty/omcUpdate_1_9_4/checkoutOpenModelica.sh
+++ dynawo/3rdParty/omcUpdate_1_9_4/checkoutOpenModelica.sh
@@ -100,8 +100,8 @@ check_tag_openmodelica() {
 check_tag_omcompiler() {
   if [ -d "$SRC_OPENMODELICA/OMCompiler" ]; then
     cd $SRC_OPENMODELICA/OMCompiler
-    last_log_omcompiler=$(git log -1 --decorate | grep -o "fix 1.9.4 version and date")
-    if [ "$last_log_omcompiler" != "fix 1.9.4 version and date" ]; then
+    last_log_omcompiler=$(git log -1 --decorate | grep -o "tag: v${OPENMODELICA_VERSION//_/.}")
+    if [ "$last_log_omcompiler" != "tag: v${OPENMODELICA_VERSION//_/.}" ]; then
       return 1
     fi
   else
@@ -129,16 +129,16 @@ check_tags() {
 
 checkout_openmodelica_repository() {
   if [ ! -d "$SRC_OPENMODELICA" ]; then
-    git clone --branch v${OPENMODELICA_VERSION//_/.} --depth 1 $DYNAWO_OPENMODELICA_GIT_URL $SRC_OPENMODELICA || error_exit "Git clone of OpenModelica in $SRC_OPENMODELICA failed."
+    git clone --branch v${OPENMODELICA_VERSION//_/.} $DYNAWO_OPENMODELICA_GIT_URL $SRC_OPENMODELICA || error_exit "Git clone of OpenModelica in $SRC_OPENMODELICA failed."
     if [ -d "$SRC_OPENMODELICA" ]; then
       cd "$SRC_OPENMODELICA"
       GIT_OPTION=""
       if check_git_version; then
         GIT_OPTION="--progress"
       fi
-      git submodule update --depth 1 --init $GIT_OPTION OMCompiler || error_exit "Git clone of OMCompiler in $SRC_OPENMODELICA failed."
-      (cd OMCompiler; git submodule update --depth 60 --init $GIT_OPTION 3rdParty;) || error_exit "Git clone of OMCompiler/3rdParty in $SRC_OPENMODELICA failed."
-      (cd OMCompiler; git submodule update --depth 50 --init $GIT_OPTION common;) || error_exit "Git clone of OMCompiler/common in $SRC_OPENMODELICA failed."
+      git submodule update --init $GIT_OPTION OMCompiler || error_exit "Git clone of OMCompiler in $SRC_OPENMODELICA failed."
+      (cd OMCompiler; git submodule update --init $GIT_OPTION 3rdParty;) || error_exit "Git clone of OMCompiler/3rdParty in $SRC_OPENMODELICA failed."
+      (cd OMCompiler; git submodule update --init $GIT_OPTION common;) || error_exit "Git clone of OMCompiler/common in $SRC_OPENMODELICA failed."
       git submodule update --init $GIT_OPTION --recursive libraries || error_exit "Git clone of libraries in $SRC_OPENMODELICA failed."
       git submodule update --init $GIT_OPTION --recursive common || error_exit "Git clone of common in $SRC_OPENMODELICA failed."
       if [ -d "$SRC_OPENMODELICA/libraries" ]; then
@@ -165,5 +165,3 @@ checkout_openmodelica_repository() {
 
 check_configuration
 checkout_openmodelica_repository
-
-exit 0
