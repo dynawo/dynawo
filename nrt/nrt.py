a04c76041f7ffd5c0c8b765d9878a34839d09717
Gautier Bureau <gautier.bureau@gmail.com>
2019-11-12 14:51:17 +0100

    #517 Added patch for macos for OpenModelica. Corrected nrt wrong import order.

--- nrt/nrt.py
+++ nrt/nrt.py
@@ -14,6 +14,7 @@
 import datetime
 import glob
 import imp
+import sys
 try:
     import lxml.etree
 except:
@@ -26,7 +27,6 @@ import re
 import signal
 import shutil
 import subprocess
-import sys
 import time
 
 try:
