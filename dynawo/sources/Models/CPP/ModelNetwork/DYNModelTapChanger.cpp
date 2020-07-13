45e40a527f3d8192afaf15fa10d0aa67ed6aef0b
e2l-su <github.com@ellipsys.fr>
2020-07-13 12:19:04 +0200

    #873 873_tap_changer_optim_inlining

--- dynawo/sources/Models/CPP/ModelNetwork/DYNModelTapChanger.cpp
+++ dynawo/sources/Models/CPP/ModelNetwork/DYNModelTapChanger.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2015-2019, RTE (http://www.rte-france.com)
+// Copyright (C) 2015-2019, RTE (http://www.rte-france.com)
 // See AUTHORS.txt
 // All rights reserved.
 // This Source Code Form is subject to the terms of the Mozilla Public
@@ -20,85 +20,23 @@
 #include "DYNModelTapChanger.h"
 #include "DYNMacrosMessage.h"
 
-using std::string;
+// using std::string;
 
 namespace DYN {
 
-ModelTapChanger::ModelTapChanger(const std::string& id) :
-id_(id),
-currentStepIndex_(0),
-regulating_(false),
-lowStepIndex_(0),
-highStepIndex_(0),
-tFirst_(60),
-tNext_(10) {
-}
+ModelTapChanger::~ModelTapChanger() {}
 
-ModelTapChanger::~ModelTapChanger() {
-}
-
-void
-ModelTapChanger::addStep(int index, const TapChangerStep& step) {
-  steps_[index] = step;
-}
-
-void
-ModelTapChanger::setCurrentStepIndex(const int& index) {
-  currentStepIndex_ = index;
-}
-
-int
-ModelTapChanger::getCurrentStepIndex() const {
-  return currentStepIndex_;
-}
-
-const TapChangerStep&
-ModelTapChanger::getStep(int key) const {
-  if (steps_.find(key) != steps_.end())
-    return steps_.find(key)->second;
-  else
-    throw DYNError(Error::MODELER, UndefinedStep, key, id_);
-}
-
-const TapChangerStep&
-ModelTapChanger::getCurrentStep() const {
-  return getStep(currentStepIndex_);
-}
-
-void
-ModelTapChanger::setLowStepIndex(const int& index) {
-  lowStepIndex_ = index;
-}
-
-void
-ModelTapChanger::setHighStepIndex(const int& index) {
-  highStepIndex_ = index;
-}
-
-void
-ModelTapChanger::setRegulating(bool regulating) {
-  regulating_ = regulating;
-}
-
-void
-ModelTapChanger::setTFirst(const double& time) {
-  tFirst_ = time;
-}
-
-void
-ModelTapChanger::setTNext(const double& time) {
-  tNext_ = time;
-}
-
-void
-ModelTapChanger::evalG(double /*t*/, double /*valueMonitored*/, bool /*nnodeOff*/, state_g* /*g*/, double /*disable*/,
-                       double /*locked*/, bool /*tfoClosed*/) {
+void ModelTapChanger::evalG(double /*t*/, double /*valueMonitored*/,
+                            bool /*nnodeOff*/, state_g* /*g*/,
+                            double /*disable*/, double /*locked*/,
+                            bool /*tfoClosed*/) {
   // not needed
 }
 
-void
-ModelTapChanger::evalZ(double /*t*/, state_g* /*g*/, ModelNetwork* /*network*/, double /*disable*/, bool /*nodeOff*/, double /*locked*/,
-                     bool /*tfoClosed*/) {
+void ModelTapChanger::evalZ(double /*t*/, state_g* /*g*/,
+                            ModelNetwork* /*network*/, double /*disable*/,
+                            bool /*nodeOff*/, double /*locked*/,
+                            bool /*tfoClosed*/) {
   // not needed
 }
 
--- dynawo/sources/Models/CPP/ModelNetwork/DYNModelTapChanger.h
+++ dynawo/sources/Models/CPP/ModelNetwork/DYNModelTapChanger.h
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2015-2019, RTE (http://www.rte-france.com)
+// Copyright (C) 2015-2019, RTE (http://www.rte-france.com)
 // See AUTHORS.txt
 // All rights reserved.
 // This Source Code Form is subject to the terms of the Mozilla Public
@@ -20,12 +20,13 @@
 #ifndef MODELS_CPP_MODELNETWORK_DYNMODELTAPCHANGER_H_
 #define MODELS_CPP_MODELNETWORK_DYNMODELTAPCHANGER_H_
 
-#include <vector>
-#include <string>
+// #include <vector>
 #include <map>
+#include <string>
 
+#include "DYNEnumUtils.h"      // stage_g
+#include "DYNMacrosMessage.h"  // Error::MODELER
 #include "DYNModelTapChangerStep.h"
-#include "DYNEnumUtils.h"
 
 namespace DYN {
 class ModelNetwork;
@@ -40,7 +41,14 @@ class ModelTapChanger {
    *
    * @param id : name of the tap changer
    */
-  explicit ModelTapChanger(const std::string& id);
+  explicit ModelTapChanger(std::string const& id)
+      : id_(id),
+        currentStepIndex_(0),
+        regulating_(false),
+        lowStepIndex_(0),
+        highStepIndex_(0),
+        tFirst_(60),
+        tNext_(10) {}
 
   /**
    * @brief destructor
@@ -51,114 +59,115 @@ class ModelTapChanger {
    * @brief  return the name of the tap changer
    * @return name of the tap changer
    */
-  inline std::string id() const {
-    return id_;
-  }
+  std::string const& id() const { return id_; }
 
   /**
-   * @brief  return the step associated to the index
+   * @brief return the step associated to the index
    * @param key associated to the index
    * @return step associated to the index
    */
-  const TapChangerStep& getStep(int key) const;
+  TapChangerStep const& getStep(int key) const {
+    if (steps_.find(key) != steps_.end()) {
+      return steps_.find(key)->second;
+    } else {
+      throw DYNError(Error::MODELER, UndefinedStep, key, id_);
+    }
+  }
 
   /**
    * @brief  add a new TapChangerStep to the steps vector
    * @param index
    * @param step
    */
-  void addStep(int index, const TapChangerStep& step);
+  void addStep(int index, TapChangerStep const& step) { steps_[index] = step; }
 
   /**
    * @brief  get the current TapChangerStep object
    * @return tap changer step
    */
-  const TapChangerStep& getCurrentStep() const;
+  const TapChangerStep& getCurrentStep() const {
+    return getStep(currentStepIndex_);
+  }
 
   /**
    * @brief  return the name of the tap changer
    * @return name of the tap changer
    */
-  inline size_t size() const {
-    return steps_.size();
-  }
+  size_t size() const { return steps_.size(); }
 
   /**
    * @brief  set the current step to a new index
    * @param index
    */
-  void setCurrentStepIndex(const int& index);
+  void setCurrentStepIndex(int const& index) { currentStepIndex_ = index; }
+
   /**
    * @brief   get the current step index
    * @return current step index
    */
-  int getCurrentStepIndex() const;
+  int getCurrentStepIndex() const { return currentStepIndex_; }
 
   /**
    * @brief   get the lowest step index
    * @return index
    */
-  inline int getLowStepIndex() const {
-    return lowStepIndex_;
-  }
+  int getLowStepIndex() const { return lowStepIndex_; }
 
   /**
    * @brief   set the lowest step index
    * @param index
    */
-  void setLowStepIndex(const int& index);
+  void setLowStepIndex(int const& index) { lowStepIndex_ = index; }
 
   /**
    * @brief   get the highest step index
    * @return index
    */
-  inline int getHighStepIndex() const {
-    return highStepIndex_;
-  }
+  int getHighStepIndex() const { return highStepIndex_; }
+
   /**
    * @brief  set the highest step index
    * @param index
    */
-  void setHighStepIndex(const int& index);
+  void setHighStepIndex(int const& index) { highStepIndex_ = index; }
+
   /**
-   * @brief   get if the tap changer is regulating
+   * @brief get if the tap changer is regulating
    * @return regulating
    */
-  inline bool getRegulating() const {
-    return regulating_;
-  }
+  bool getRegulating() const { return regulating_; }
+
   /**
-   * @brief   set if the tap changer is regulating
+   * @brief set if the tap changer is regulating
    * @param regulating
    */
-  void setRegulating(bool regulating);
+  void setRegulating(bool regulating) { regulating_ = regulating; }
 
   /**
    * @brief   get the time to wait before changing of step for the first time
    * @return time
    */
-  inline double getTFirst() const {
-    return tFirst_;
-  }
+  double getTFirst() const { return tFirst_; }
 
   /**
    * @brief   set the time to wait before changing of step for the first time
    * @param time
    */
-  void setTFirst(const double& time);
+  void setTFirst(double const& time) { tFirst_ = time; }
 
   /**
-   * @brief   get the time to wait before changing of step if it's not the first time
+   * @brief   get the time to wait before changing of step if it's not the first
+   * time
    * @return time
    */
-  inline double getTNext() const {
-    return tNext_;
-  }
+  double getTNext() const { return tNext_; }
+
   /**
-   * @brief  set the time to wait before changing of step if it's not the first time
+   * @brief  set the time to wait before changing of step if it's not the first
+   * time
    * @param time
    */
-  void setTNext(const double& time);
+  void setTNext(double const& time) { tNext_ = time; }
 
  public:
   /**
@@ -171,8 +180,8 @@ class ModelTapChanger {
    * @param locked : is the tap changer locked ?
    * @param tfoClosed : is the transformer connected ?
    */
-  virtual void evalG(double t, double valueMonitored, bool nodeOff, state_g* g, double disable, double locked,
-                     bool tfoClosed);
+  virtual void evalG(double t, double valueMonitored, bool nodeOff, state_g* g,
+                     double disable, double locked, bool tfoClosed);
 
   /**
    * @brief  evaluate discrete values
@@ -184,35 +193,34 @@ class ModelTapChanger {
    * @param locked : is the tap changer locked ?
    * @param tfoClosed :is the transformer connected ?
    */
-  virtual void evalZ(double t, state_g* g, ModelNetwork* network, double disable, bool nodeOff, double locked,
+  virtual void evalZ(double t, state_g* g, ModelNetwork* network,
+                     double disable, bool nodeOff, double locked,
                      bool tfoClosed);
 
   /**
    * @brief  get the size of the local G function
    * @return size of G function
    */
-  virtual int sizeG() const {
-    return 0;
-  }
+  virtual int sizeG() const { return 0; }
 
   /**
    * @brief  get size of discrete variables
    * @return number of discrete variables
    */
-  virtual int sizeZ() const {
-    return 0;
-  }
+  virtual int sizeZ() const { return 0; }
 
  private:
   std::string id_;  ///< id of the tap changer
-  std::map<int, TapChangerStep> steps_;  ///< map of TapChangerStep : index -> step
+  std::map<int, TapChangerStep>
+      steps_;             ///< map of TapChangerStep : index -> step
   int currentStepIndex_;  ///< index of the current step
-  bool regulating_;  ///< is the tapChanger regulating ?
-  int lowStepIndex_;  ///< Lowest step
-  int highStepIndex_;  ///< Highest step
+  bool regulating_;       ///< is the tapChanger regulating ?
+  int lowStepIndex_;      ///< Lowest step
+  int highStepIndex_;     ///< Highest step
   double tFirst_;  ///< time to wait before changing of step for the first time
-  double tNext_;  ///< time to wait before changing of step if it's not the first time
-};
+  double tNext_;   ///< time to wait before changing of step if it's not the
+                   ///< first time
+};                 // class ModelTapChanger
 }  // namespace DYN
 
 #endif  // MODELS_CPP_MODELNETWORK_DYNMODELTAPCHANGER_H_
