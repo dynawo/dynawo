a5c89ee02db9764abe263c944fb82e39330aa914
e2l-su <github.com@ellipsys.fr>
2020-07-16 16:17:03 +0200

    #873 873: inlining the Tap CHanger

--- dynawo/sources/Models/CPP/ModelNetwork/DYNModelPhaseTapChanger.h
+++ dynawo/sources/Models/CPP/ModelNetwork/DYNModelPhaseTapChanger.h
@@ -20,6 +20,7 @@
 #ifndef MODELS_CPP_MODELNETWORK_DYNMODELPHASETAPCHANGER_H_
 #define MODELS_CPP_MODELNETWORK_DYNMODELPHASETAPCHANGER_H_
 
+#include "DYNModelConstants.h"
 #include "DYNModelTapChanger.h"
 
 namespace DYN {
@@ -34,12 +35,22 @@ class ModelPhaseTapChanger : public ModelTapChanger {
    *
    * @param id : name of the tap changer
    */
-  explicit ModelPhaseTapChanger(const std::string& id);
+  inline explicit ModelPhaseTapChanger(const std::string& id)
+      : ModelTapChanger(id),
+        thresholdI_(0),
+        whenUp_(VALDEF),
+        whenDown_(VALDEF),
+        whenLastTap_(VALDEF),
+        moveUp_(false),
+        moveDown_(false),
+        tapRefDown_(-1),
+        tapRefUp_(-1),
+        currentOverThresholdState_(false) {}
 
   /**
-   * @brief destructor
+   * @brief  destructor
    */
-  ~ModelPhaseTapChanger();
+  virtual ~ModelPhaseTapChanger() {}
 
   /**
    * @brief  evaluate the zero crossing functions
@@ -52,7 +63,8 @@ class ModelPhaseTapChanger : public ModelTapChanger {
    * @param locked : is the tap changer locked ?
    * @param tfoClosed : is the transformer connected ?
    */
-  void evalG(double t, double iValue, bool nodeOff, state_g* g, double disable, double locked, bool tfoClosed);
+  virtual void evalG(double t, double iValue, bool nodeOff, state_g* g,
+                     double disable, double locked, bool tfoClosed);
 
   /**
    * @brief  evaluate discrete values
@@ -60,32 +72,36 @@ class ModelPhaseTapChanger : public ModelTapChanger {
    * @param g: root values
    * @param network : network of the transformer
    * @param disable : is the tap changer disabled ?
-   * @param P1SupP2 : is the active power evaluated at side 1 is superior to the active power evaluated at side 2  ?
+   * @param P1SupP2 : is the active power evaluated at side 1 is superior to the
+   * active power evaluated at side 2  ?
    * @param locked : is the tap changer locked ?
    * @param tfoClosed :is the transformer connected ?
    */
-  void evalZ(double t, state_g* g, ModelNetwork* network, double disable, bool P1SupP2, double locked, bool tfoClosed);
+  virtual void evalZ(double t, state_g* g, ModelNetwork* network,
+                     double disable, bool P1SupP2, double locked,
+                     bool tfoClosed);
 
   /**
    * @copydoc ModelTapChanger::sizeG()
    */
-  int sizeG() const;
+  virtual int sizeG() const { return 6; }
 
   /**
    * @copydoc ModelTapChanger::sizeZ()
    */
-  int sizeZ() const;
+  virtual int sizeZ() const { return 0; }
 
   /**
    * @brief set the current threshold over which the current should not go
    *
    * @param threshold current threshold
    */
-  void setThresholdI(const double& threshold);
+  inline void setThresholdI(double threshold) { thresholdI_ = threshold; }
 
  private:
   /**
-   * @brief decide whether we should increase/decrease tap depending on tap description and power flow
+   * @brief decide whether we should increase/decrease tap depending on tap
+   * description and power flow
    * @param P1SupP2 @b true if P at side 1 is superior to P at side 2
    * @return @b true if a tap up increase the phase
    */
@@ -93,14 +109,15 @@ class ModelPhaseTapChanger : public ModelTapChanger {
 
  private:
   double thresholdI_;  ///< threshold of I
-  double whenUp_;  ///< when the current reached a value over the threshold
-  double whenDown_;  ///< when the current reached a value under the threshold
+  double whenUp_;      ///< when the current reached a value over the threshold
+  double whenDown_;    ///< when the current reached a value under the threshold
   double whenLastTap_;  ///< last time when a tap changer
-  bool moveUp_;  ///< @b true if tap should be increased
-  bool moveDown_;  ///< @b false if tap should be decreased
-  int tapRefDown_;  ///< initial tap when trying to decrease tap
-  int tapRefUp_;  ///<  initial tap when trying to increase tap
-  bool currentOverThresholdState_;  ///< @b true if the current is over the threshold
+  bool moveUp_;         ///< @b true if tap should be increased
+  bool moveDown_;       ///< @b false if tap should be decreased
+  int tapRefDown_;      ///< initial tap when trying to decrease tap
+  int tapRefUp_;        ///<  initial tap when trying to increase tap
+  bool currentOverThresholdState_;  ///< @b true if the current is over the
+                                    ///< threshold
 };
 }  // namespace DYN
 
--- dynawo/sources/Models/CPP/ModelNetwork/DYNModelRatioTapChanger.h
+++ dynawo/sources/Models/CPP/ModelNetwork/DYNModelRatioTapChanger.h
@@ -20,6 +20,7 @@
 #ifndef MODELS_CPP_MODELNETWORK_DYNMODELRATIOTAPCHANGER_H_
 #define MODELS_CPP_MODELNETWORK_DYNMODELRATIOTAPCHANGER_H_
 
+#include "DYNModelConstants.h"
 #include "DYNModelTapChanger.h"
 
 namespace DYN {
@@ -35,12 +36,27 @@ class ModelRatioTapChanger : public ModelTapChanger {
    * @param id : name of the tap changer
    * @param side : side where the voltage is controlled
    */
-  explicit ModelRatioTapChanger(const std::string& id, const std::string& side);
+  inline explicit ModelRatioTapChanger(const std::string& id,
+                                       const std::string& side)
+      : ModelTapChanger(id),
+        side_(side),
+        tolV_(0.015),
+        targetV_(0),
+        whenUp_(VALDEF),
+        whenDown_(VALDEF),
+        whenLastTap_(VALDEF),
+        moveUp_(false),
+        moveDown_(false),
+        tapRefDown_(-1),
+        tapRefUp_(-1),
+        uMaxState_(false),
+        uMinState_(false),
+        uTargetState_(true) {}
 
   /**
    * @brief destructor
    */
-  ~ModelRatioTapChanger();
+  virtual ~ModelRatioTapChanger() {}
 
   /**
    * @brief  evaluate the zero crossing functions
@@ -53,75 +69,80 @@ class ModelRatioTapChanger : public ModelTapChanger {
    * @param locked : is the tap changer locked ?
    * @param tfoClosed : is the transformer connected ?
    */
-  void evalG(double t, double uValue, bool nodeOff, state_g* g, double disable, double locked, bool tfoClosed);
+  virtual void evalG(double t, double uValue, bool nodeOff, state_g* g,
+                     double disable, double locked, bool tfoClosed);
 
   /**
    * @copydoc ModelTapChanger::evalZ(double t, state_g* rootFound, ModelNetwork* network, double disable, bool nodeOff, double locked, bool tfoClosed)
    */
-  void evalZ(double t, state_g* g, ModelNetwork* network, double disable, bool nodeOff, double locked, bool tfoClosed);
+  virtual void evalZ(double t, state_g* g, ModelNetwork* network,
+                     double disable, bool nodeOff, double locked,
+                     bool tfoClosed);
 
   /**
    * @brief  get the size of the local G function
    * @return size of G function
    */
-  int sizeG() const;
+  virtual int sizeG() const { return 7; }
 
   /**
    * @brief  get size of discrete variables
    * @return number of discrete variables
    */
-  int sizeZ() const;
+  virtual int sizeZ() const { return 0; }
 
   /**
    * @brief set the dead band around the target of the tap changer
    *
    * @param tolerance dead band to use
    */
-  void setTolV(const double& tolerance);
+  void setTolV(const double& tolerance) { tolV_ = tolerance; }
 
   /**
    * @brief set the target of the tap changer
    *
    * @param target target to use
    */
-  void setTargetV(const double& target);
+  void setTargetV(const double& target) { targetV_ = target; }
 
   /**
    * @brief get the current dead band of the tap changer
    * @return value of the current dead band
    */
-  double getTolV() const;
+  double getTolV() const { return tolV_; }
 
   /**
    * @brief set the reference side
    *
    * @param side reference side
    */
-  void setSide(std::string side) {
-    side_ = side;
-  }
+  inline void setSide(const std::string& side) { side_ = side; }
 
  private:
   /**
-   * @brief decide whether if the tap changer should increase/decrease tap to increase the target U
+   * @brief decide whether if the tap changer should increase/decrease tap to
+   * increase the target U
    * @return @b true if one tap up increase the voltage
    */
   bool getUpIncreaseTargetU();
 
  private:
   std::string side_;  ///< reference side where the voltage is controlled
-  double tolV_;  ///< dead band around targetV
-  double targetV_;  ///< target voltage
-  double whenUp_;  ///< when the voltage reached a value over the target+deadBand
-  double whenDown_;  ///< when the voltage reached a value under the target-deadBand
+  double tolV_;       ///< dead band around targetV
+  double targetV_;    ///< target voltage
+  double
+      whenUp_;  ///< when the voltage reached a value over the target+deadBand
+  double whenDown_;     ///< when the voltage reached a value under the
+                        ///< target-deadBand
   double whenLastTap_;  ///< last time when a tap changer
-  bool moveUp_;  ///< @b true if tap should be increased
-  bool moveDown_;  ///< @b false if tap should be decreased
-  int tapRefDown_;  ///< initial tap when trying to decrease tap
-  int tapRefUp_;  ///<  initial tap when trying to increase tap
-  bool uMaxState_;  ///< @b true if U > uTarget + uDeadBand
-  bool uMinState_;  ///< @b true if U < uTarget - uDeadBand
-  bool uTargetState_;  ///< @b true if uTarget - uDeadBand < U < uTarget + uDeadBand
+  bool moveUp_;         ///< @b true if tap should be increased
+  bool moveDown_;       ///< @b false if tap should be decreased
+  int tapRefDown_;      ///< initial tap when trying to decrease tap
+  int tapRefUp_;        ///<  initial tap when trying to increase tap
+  bool uMaxState_;      ///< @b true if U > uTarget + uDeadBand
+  bool uMinState_;      ///< @b true if U < uTarget - uDeadBand
+  bool uTargetState_;   ///< @b true if uTarget - uDeadBand < U < uTarget +
+                        ///< uDeadBand
 };
 }  // namespace DYN
 
