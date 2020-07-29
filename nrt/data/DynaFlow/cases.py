9ad11662c5042a47d1935d1e520d55e7d00c6a88
GUIRONNET Adrien <adrien.guironnet@rte-france.com>
2020-07-29 10:07:43 +0200

    #1023 Examples refactoring for the release

---
+++ nrt/data/DynaFlow/cases.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015-2020, RTE (http://www.rte-france.com)
+# See AUTHORS.txt
+# All rights reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+#
+# This file is part of Dynawo, an hybrid C++/Modelica open source time domain
+# simulation tool for power systems.
+
+import os
+
+test_cases = []
+standardReturnCode = [0]
+standardReturnCodeType = "ALLOWED"
+forbiddenReturnCodeType = "FORBIDDEN"
+
+#####################################
+#     IEEE 14 - Disconnect Line     #
+#####################################
+
+case_name = "DynaFlow - IEEE14 - DisconnectLine"
+case_description = "IEEE 14 test case with a line disconnection"
+job_file = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))), "examples", "DynaFlow", "IEEE14", "IEEE14_DisconnectLine", "IEEE14.jobs")
+
+test_cases.append((case_name, case_description, job_file, 1, standardReturnCodeType, standardReturnCode))
+
+##############################################
+#  Small System with an HVDC link and a CLA  #
+##############################################
+
+case_name = "DynaFlow - HVDC and CLA"
+case_description = "Small System with an HVDC link and a CLA"
+job_file = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))), "examples", "DynaFlow", "IllustrativeExamples", "HVDC_CLA", "HVDC_CLA.jobs")
+
+test_cases.append((case_name, case_description, job_file, 1, standardReturnCodeType, standardReturnCode))
+
+########################################################
+#  Small System with an HVDC link and a Phase Shifter  #
+########################################################
+
+case_name = "DynaFlow - HVDC and PhaseShifter"
+case_description = "Small System with an HVDC link and a Phase Shifter"
+job_file = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))), "examples", "DynaFlow", "IllustrativeExamples", "HVDC_PhaseShifter", "HVDC_PhaseShifter.jobs")
+
+test_cases.append((case_name, case_description, job_file, 1, standardReturnCodeType, standardReturnCode))
+
+##########################################
+#  Small System with a Load Restoration  #
+##########################################
+
+case_name = "DynaFlow - LoadRestoration"
+case_description = "Small System with a Load Restoration"
+job_file = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))), "examples", "DynaFlow", "IllustrativeExamples", "LoadRestoration", "LoadRestoration.jobs")
+
+test_cases.append((case_name, case_description, job_file, 1, standardReturnCodeType, standardReturnCode))
+
+##########################################
+#  Small System with two Phase Shifters  #
+##########################################
+
+case_name = "DynaFlow - PhaseShifters"
+case_description = "Small System with two Phase Shifters"
+job_file = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))), "examples", "DynaFlow", "IllustrativeExamples", "PhaseShifters", "PhaseShifters.jobs")
+
+test_cases.append((case_name, case_description, job_file, 1, standardReturnCodeType, standardReturnCode))
---
+++ nrt/data/DynaWaltz/cases.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015-2020, RTE (http://www.rte-france.com)
+# See AUTHORS.txt
+# All rights reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+#
+# This file is part of Dynawo, an hybrid C++/Modelica open source time domain
+# simulation tool for power systems.
+
+import os
+
+test_cases = []
+standardReturnCode = [0]
+standardReturnCodeType = "ALLOWED"
+forbiddenReturnCodeType = "FORBIDDEN"
+
+#################################################
+#     IEEE14 - Generator disconnections         #
+#################################################
+case_name = "DynaWaltz - IEEE14 - Generator disconnections"
+case_description = "IEEE14 - Generator disconnections"
+job_file = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))), "examples", "DynaWaltz", "IEEE14", "IEEE14_GeneratorDisconnections", "IEEE14.jobs")
+
+test_cases.append((case_name, case_description, job_file, 1, standardReturnCodeType, standardReturnCode))
+
+####################################################################
+#     IEEE14 - Cascading line tripping                             #
+####################################################################
+
+case_name = "DynaWaltz - IEEE14 - Cascading line tripping"
+case_description = "IEEE14 - Cascading line tripping"
+job_file = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))), "examples", "DynaWaltz", "IEEE14", "IEEE14_CascadingLineTrippings", "IEEE14.jobs")
+
+test_cases.append((case_name, case_description, job_file, 1, standardReturnCodeType, standardReturnCode))
+
+####################################################################
+#     IEEE57 - Generator disconnection                             #
+####################################################################
+
+case_name = "DynaWaltz - IEEE57 - Generator disconnection"
+case_description = "IEEE57 - Generator disconnection"
+job_file = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))), "examples", "DynaWaltz", "IEEE57", "IEEE57_GeneratorDisconnection", "IEEE57.jobs")
+
+test_cases.append((case_name, case_description, job_file, 1, standardReturnCodeType, standardReturnCode))
