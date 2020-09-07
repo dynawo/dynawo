d4148e83b790c6a7958489b3bc66c3d03cde1206
Le Courtois Florent <florent.lecourtois@rte-france.com>
2020-09-07 11:20:41 +0200

    #1100 Define missing copy assignement operator

--- dynawo/sources/Common/DYNTraceStream.cpp
+++ dynawo/sources/Common/DYNTraceStream.cpp
@@ -24,7 +24,7 @@
 namespace DYN {
 
 TraceStream::TraceStream() :
-buffer_(),
+buffer_{boost::shared_ptr<std::stringstream>(new std::stringstream)},
 slv_(INFO),
 tag_("") {
   buffer_ = boost::shared_ptr<std::stringstream>(new std::stringstream);
@@ -40,9 +40,18 @@ tag_(tag) {
 }
 
 TraceStream::TraceStream(const TraceStream& ts) :
-buffer_(),
+buffer_{boost::shared_ptr<std::stringstream>(new std::stringstream)},
 slv_(ts.slv_),
 tag_(ts.tag_) {
+}
+
+TraceStream&
+TraceStream::operator=(const TraceStream& ts) {
+  slv_ = ts.slv_;
+  tag_ = ts.tag_;
+  buffer_.reset();
+  buffer_ = boost::shared_ptr<std::stringstream>(new std::stringstream);
+  return *this;
 }
 
 TraceStream::~TraceStream() {
