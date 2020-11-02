3f9d58db78f65af582e52da22a4e47ede967bbf1
e2l-su <github.com@ellipsys.fr>
2020-11-02 06:50:14 +0100

    #1029 DYN::HvdcLineInterface and unit test

---
+++ dynawo/sources/Modeler/DataInterface/PowSyblIIDM/DYNHvdcLineInterfaceIIDM.cpp
@@ -0,0 +1,219 @@
+//
+// Copyright (c) 2015-2020, RTE (http://www.rte-france.com)
+// See AUTHORS.txt
+// All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, you can obtain one at http://mozilla.org/MPL/2.0/.
+// SPDX-License-Identifier: MPL-2.0
+//
+// This file is part of Dynawo, an hybrid C++/Modelica open source time domain
+// simulation tool for power systems.
+//
+
+//======================================================================
+/**
+ * @file  DYNHvdcLineInterfaceIIDM.cpp
+ *
+ * @brief Hvdc Line data interface : implementation file for IIDM interface
+ *
+ */
+//======================================================================
+
+#include "DYNHvdcLineInterfaceIIDM.h"
+
+#include <powsybl/iidm/HvdcLine.hpp>
+
+#include "DYNLccConverterInterfaceIIDM.h"
+#include "DYNModelConstants.h"
+#include "DYNVscConverterInterfaceIIDM.h"
+
+#include <boost/pointer_cast.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include <string>
+
+using boost::dynamic_pointer_cast;
+using boost::shared_ptr;
+using std::string;
+
+namespace DYN {
+
+HvdcLineInterfaceIIDM::HvdcLineInterfaceIIDM(powsybl::iidm::HvdcLine& hvdcLine,
+                                             const shared_ptr<ConverterInterface>& conv1,
+                                             const shared_ptr<ConverterInterface>& conv2) : hvdcLineIIDM_(hvdcLine),
+                                                                                            conv1_(conv1),
+                                                                                            conv2_(conv2) {
+  setType(ComponentInterface::HVDC_LINE);
+  stateVariables_.resize(6);
+  stateVariables_[VAR_P1] = StateVariable("p1", StateVariable::DOUBLE);       // P1
+  stateVariables_[VAR_P2] = StateVariable("p2", StateVariable::DOUBLE);       // P2
+  stateVariables_[VAR_Q1] = StateVariable("q1", StateVariable::DOUBLE);       // Q1
+  stateVariables_[VAR_Q2] = StateVariable("q2", StateVariable::DOUBLE);       // Q2
+  stateVariables_[VAR_STATE1] = StateVariable("state1", StateVariable::INT);  // connectionState1
+  stateVariables_[VAR_STATE2] = StateVariable("state2", StateVariable::INT);  // connectionState2
+}
+
+HvdcLineInterfaceIIDM::~HvdcLineInterfaceIIDM() {
+}
+
+void HvdcLineInterfaceIIDM::exportStateVariablesUnitComponent() {
+  switch (conv1_->getConverterType()) {
+    case ConverterInterface::VSC_CONVERTER: {
+      shared_ptr<VscConverterInterfaceIIDM> vsc1 = dynamic_pointer_cast<VscConverterInterfaceIIDM>(conv1_);
+      shared_ptr<VscConverterInterfaceIIDM> vsc2 = dynamic_pointer_cast<VscConverterInterfaceIIDM>(conv2_);
+      (vsc1->getVscIIDM()).getTerminal().setP(-1 * getValue<double>(VAR_P1) * SNREF);
+      (vsc1->getVscIIDM()).getTerminal().setQ(-1 * getValue<double>(VAR_Q1) * SNREF);
+      (vsc2->getVscIIDM()).getTerminal().setP(-1 * getValue<double>(VAR_P2) * SNREF);
+      (vsc2->getVscIIDM()).getTerminal().setQ(-1 * getValue<double>(VAR_Q2) * SNREF);
+      bool connected1 = (getValue<int>(VAR_STATE1) == CLOSED);
+      bool connected2 = (getValue<int>(VAR_STATE2) == CLOSED);
+      if ((vsc1->getVscIIDM()).getTerminal().isConnected()) {
+        if (connected1)
+          (vsc1->getVscIIDM()).getTerminal().connect();
+        else
+          (vsc1->getVscIIDM()).getTerminal().disconnect();
+      }
+      if ((vsc2->getVscIIDM()).getTerminal().isConnected()) {
+        if (connected2)
+          (vsc2->getVscIIDM()).getTerminal().connect();
+        else
+          (vsc2->getVscIIDM()).getTerminal().disconnect();
+      }
+      break;
+    }
+    case ConverterInterface::LCC_CONVERTER: {
+      shared_ptr<LccConverterInterfaceIIDM> lcc1 = dynamic_pointer_cast<LccConverterInterfaceIIDM>(conv1_);
+      shared_ptr<LccConverterInterfaceIIDM> lcc2 = dynamic_pointer_cast<LccConverterInterfaceIIDM>(conv2_);
+      (lcc1->getLccIIDM()).getTerminal().setP(-1 * getValue<double>(VAR_P1) * SNREF);
+      (lcc1->getLccIIDM()).getTerminal().setQ(-1 * getValue<double>(VAR_Q1) * SNREF);
+      (lcc2->getLccIIDM()).getTerminal().setP(-1 * getValue<double>(VAR_P2) * SNREF);
+      (lcc2->getLccIIDM()).getTerminal().setQ(-1 * getValue<double>(VAR_Q2) * SNREF);
+      bool connected1 = (getValue<int>(VAR_STATE1) == CLOSED);
+      bool connected2 = (getValue<int>(VAR_STATE2) == CLOSED);
+      if ((lcc1->getLccIIDM()).getTerminal().isConnected()) {
+        if (connected1)
+          (lcc1->getLccIIDM()).getTerminal().connect();
+        else
+          (lcc1->getLccIIDM()).getTerminal().disconnect();
+      }
+      if ((lcc2->getLccIIDM()).getTerminal().isConnected()) {
+        if (connected2)
+          (lcc2->getLccIIDM()).getTerminal().connect();
+        else
+          (lcc2->getLccIIDM()).getTerminal().disconnect();
+      }
+      break;
+    }
+  }
+}
+
+void HvdcLineInterfaceIIDM::importStaticParameters() {
+  staticParameters_.clear();
+  staticParameters_.insert(std::make_pair("p1_pu", StaticParameter("p1_pu", StaticParameter::DOUBLE).setValue(conv1_->getP() / SNREF)));
+  staticParameters_.insert(std::make_pair("q1_pu", StaticParameter("q1_pu", StaticParameter::DOUBLE).setValue(conv1_->getQ() / SNREF)));
+  staticParameters_.insert(std::make_pair("p1", StaticParameter("p1", StaticParameter::DOUBLE).setValue(conv1_->getP())));
+  staticParameters_.insert(std::make_pair("q1", StaticParameter("q1", StaticParameter::DOUBLE).setValue(conv1_->getQ())));
+  staticParameters_.insert(std::make_pair("p2_pu", StaticParameter("p2_pu", StaticParameter::DOUBLE).setValue(conv2_->getP() / SNREF)));
+  staticParameters_.insert(std::make_pair("q2_pu", StaticParameter("q2_pu", StaticParameter::DOUBLE).setValue(conv2_->getQ() / SNREF)));
+  staticParameters_.insert(std::make_pair("p2", StaticParameter("p2", StaticParameter::DOUBLE).setValue(conv2_->getP())));
+  staticParameters_.insert(std::make_pair("q2", StaticParameter("q2", StaticParameter::DOUBLE).setValue(conv2_->getQ())));
+  if (conv1_->getBusInterface()) {
+    double U10 = conv1_->getBusInterface()->getV0();
+    double U1Nom = conv1_->getVNom();
+    double teta1 = conv1_->getBusInterface()->getAngle0();
+    staticParameters_.insert(std::make_pair("v1", StaticParameter("v1", StaticParameter::DOUBLE).setValue(U10)));
+    staticParameters_.insert(std::make_pair("angle1", StaticParameter("angle1", StaticParameter::DOUBLE).setValue(teta1)));
+    staticParameters_.insert(std::make_pair("v1_pu", StaticParameter("v1_pu", StaticParameter::DOUBLE).setValue(U10 / U1Nom)));
+    staticParameters_.insert(std::make_pair("angle1_pu", StaticParameter("angle1_pu", StaticParameter::DOUBLE).setValue(teta1 * M_PI / 180)));
+  } else {
+    staticParameters_.insert(std::make_pair("v1", StaticParameter("v1", StaticParameter::DOUBLE).setValue(0.)));
+    staticParameters_.insert(std::make_pair("angle1", StaticParameter("angle1", StaticParameter::DOUBLE).setValue(0.)));
+    staticParameters_.insert(std::make_pair("v1_pu", StaticParameter("v1_pu", StaticParameter::DOUBLE).setValue(0.)));
+    staticParameters_.insert(std::make_pair("angle1_pu", StaticParameter("angle1_pu", StaticParameter::DOUBLE).setValue(0.)));
+  }
+  if (conv2_->getBusInterface()) {
+    double U20 = conv2_->getBusInterface()->getV0();
+    double U2Nom = conv2_->getVNom();
+    double teta2 = conv2_->getBusInterface()->getAngle0();
+    staticParameters_.insert(std::make_pair("v2", StaticParameter("v2", StaticParameter::DOUBLE).setValue(U20)));
+    staticParameters_.insert(std::make_pair("angle2", StaticParameter("angle2", StaticParameter::DOUBLE).setValue(teta2)));
+    staticParameters_.insert(std::make_pair("v2_pu", StaticParameter("v2_pu", StaticParameter::DOUBLE).setValue(U20 / U2Nom)));
+    staticParameters_.insert(std::make_pair("angle2_pu", StaticParameter("angle2_pu", StaticParameter::DOUBLE).setValue(teta2 * M_PI / 180)));
+
+  } else {
+    staticParameters_.insert(std::make_pair("v2", StaticParameter("v2", StaticParameter::DOUBLE).setValue(0.)));
+    staticParameters_.insert(std::make_pair("angle2", StaticParameter("angle2", StaticParameter::DOUBLE).setValue(0.)));
+    staticParameters_.insert(std::make_pair("v2_pu", StaticParameter("v2_pu", StaticParameter::DOUBLE).setValue(0.)));
+    staticParameters_.insert(std::make_pair("angle2_pu", StaticParameter("angle2_pu", StaticParameter::DOUBLE).setValue(0.)));
+  }
+}
+
+string
+HvdcLineInterfaceIIDM::getID() const {
+  return hvdcLineIIDM_.getId();
+}
+
+double
+HvdcLineInterfaceIIDM::getResistanceDC() const {
+  return hvdcLineIIDM_.getR();
+}
+
+double
+HvdcLineInterfaceIIDM::getVNom() const {
+  return hvdcLineIIDM_.getNominalVoltage();
+}
+
+double
+HvdcLineInterfaceIIDM::getActivePowerSetpoint() const {
+  return hvdcLineIIDM_.getActivePowerSetpoint();
+}
+
+double
+HvdcLineInterfaceIIDM::getPmax() const {
+  return hvdcLineIIDM_.getMaxP();
+}
+
+HvdcLineInterfaceIIDM::ConverterMode_t
+HvdcLineInterfaceIIDM::getConverterMode() const {
+  return hvdcLineIIDM_.getConvertersMode() == powsybl::iidm::HvdcLine::ConvertersMode::SIDE_1_RECTIFIER_SIDE_2_INVERTER ? HvdcLineInterface::RECTIFIER_INVERTER : HvdcLineInterface::INVERTER_RECTIFIER;
+}
+
+string
+HvdcLineInterfaceIIDM::getIdConverter1() const {
+  return hvdcLineIIDM_.getConverterStation1().get().getId();
+}
+
+string
+HvdcLineInterfaceIIDM::getIdConverter2() const {
+  return hvdcLineIIDM_.getConverterStation2().get().getId();
+}
+
+int HvdcLineInterfaceIIDM::getComponentVarIndex(const std::string& varName) const {
+  int index = -1;
+  if (varName == "p1")
+    index = VAR_P1;
+  else if (varName == "q1")
+    index = VAR_Q1;
+  else if (varName == "p2")
+    index = VAR_P2;
+  else if (varName == "q2")
+    index = VAR_Q2;
+  else if (varName == "state1")
+    index = VAR_STATE1;
+  else if (varName == "state2")
+    index = VAR_STATE2;
+  return index;
+}
+
+const shared_ptr<ConverterInterface>&
+HvdcLineInterfaceIIDM::getConverter1() const {
+  return conv1_;
+}
+
+const shared_ptr<ConverterInterface>&
+HvdcLineInterfaceIIDM::getConverter2() const {
+  return conv2_;
+}
+
+}  // namespace DYN
---
+++ dynawo/sources/Modeler/DataInterface/PowSyblIIDM/test/TestHvdcLineInterface.cpp
@@ -0,0 +1,131 @@
+//
+// Copyright (c) 2015-2020, RTE (http://www.rte-france.com)
+// See AUTHORS.txt
+// All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, you can obtain one at http://mozilla.org/MPL/2.0/.
+// SPDX-License-Identifier: MPL-2.0
+//
+// This file is part of Dynawo, an hybrid C++/Modelica open source time domain
+// simulation tool for power systems.
+//
+
+#include "DYNHvdcLineInterfaceIIDM.h"
+
+#include "DYNLccConverterInterfaceIIDM.h"
+#include "DYNVscConverterInterfaceIIDM.h"
+
+#include <powsybl/iidm/Bus.hpp>
+#include <powsybl/iidm/HvdcLine.hpp>
+#include <powsybl/iidm/HvdcLineAdder.hpp>
+#include <powsybl/iidm/LccConverterStation.hpp>
+#include <powsybl/iidm/LccConverterStationAdder.hpp>
+#include <powsybl/iidm/Network.hpp>
+#include <powsybl/iidm/Substation.hpp>
+#include <powsybl/iidm/Terminal.hpp>
+#include <powsybl/iidm/VscConverterStation.hpp>
+#include <powsybl/iidm/VscConverterStationAdder.hpp>
+
+#include "gtest_dynawo.h"
+
+namespace powsybl {
+namespace iidm {
+
+Network
+createHvdcLineNetwork() {
+  Network network("test", "test");
+  Substation& substation = network.newSubstation().setId("S1").setName("S1_NAME").setCountry(Country::FR).setTso("TSO").add();
+
+  VoltageLevel& vl1 = substation.newVoltageLevel()
+                          .setId("VL1")
+                          .setName("VL1_NAME")
+                          .setTopologyKind(TopologyKind::BUS_BREAKER)
+                          .setNominalVoltage(380.0)
+                          .setLowVoltageLimit(340.0)
+                          .setHighVoltageLimit(420.0)
+                          .add();
+
+  Bus& vl1Bus1 = vl1.getBusBreakerView().newBus().setId("VL1_BUS1").add();
+
+  vl1.newLccConverterStation()
+      .setId("LCC1")
+      .setName("LCC1_NAME")
+      .setBus(vl1Bus1.getId())
+      .setConnectableBus(vl1Bus1.getId())
+      .setLossFactor(2.0)
+      .setPowerFactor(-.2)
+      .add();
+
+  vl1.newVscConverterStation()
+      .setId("VSC2")
+      .setName("VSC2_NAME")
+      .setBus(vl1Bus1.getId())
+      .setConnectableBus(vl1Bus1.getId())
+      .setLossFactor(3.0)
+      .setVoltageRegulatorOn(true)
+      .setVoltageSetpoint(1.2)
+      .setReactivePowerSetpoint(-1.5)
+      .add();
+
+  network.newHvdcLine()
+      .setId("HVDC1")
+      .setName("HVDC1_NAME")
+      .setActivePowerSetpoint(111.1)
+      .setConvertersMode(HvdcLine::ConvertersMode::SIDE_1_RECTIFIER_SIDE_2_INVERTER)
+      .setConverterStationId1("LCC1")
+      .setConverterStationId2("VSC2")
+      .setMaxP(12.0)
+      .setNominalVoltage(13.0)
+      .setR(14.0)
+      .add();
+
+  return network;
+}  // createHvdcLineNetwork()
+}  // namespace iidm
+}  // namespace powsybl
+
+namespace DYN {
+
+using powsybl::iidm::createHvdcLineNetwork;
+
+TEST(DataInterfaceTest, HvdcLine) {
+  powsybl::iidm::Network network = createHvdcLineNetwork();
+  powsybl::iidm::HvdcLine& hvdcLine = network.getHvdcLine("HVDC1");
+  powsybl::iidm::LccConverterStation& lcc = network.getLccConverterStation("LCC1");
+  powsybl::iidm::VscConverterStation& vsc = network.getVscConverterStation("VSC2");
+
+  const boost::shared_ptr<LccConverterInterface> LccIfce(new LccConverterInterfaceIIDM(lcc));
+  const boost::shared_ptr<VscConverterInterface> VscIfce(new VscConverterInterfaceIIDM(vsc));
+
+  DYN::HvdcLineInterfaceIIDM Ifce(hvdcLine, LccIfce, VscIfce);
+  ASSERT_EQ(Ifce.getID(), "HVDC1");
+
+  ASSERT_EQ(Ifce.getIdConverter1(), "LCC1");
+  ASSERT_EQ(Ifce.getConverter1().get()->getID(), "LCC1");
+  ASSERT_EQ(Ifce.getIdConverter2(), "VSC2");
+  ASSERT_EQ(Ifce.getConverter2().get()->getID(), "VSC2");
+
+  ASSERT_DOUBLE_EQ(Ifce.getResistanceDC(), 14.0);
+  ASSERT_DOUBLE_EQ(Ifce.getVNom(), 13.0);
+  ASSERT_DOUBLE_EQ(Ifce.getPmax(), 12.0);
+  ASSERT_DOUBLE_EQ(Ifce.getActivePowerSetpoint(), 111.1);
+
+  ASSERT_EQ(Ifce.getComponentVarIndex(std::string("p1")), HvdcLineInterfaceIIDM::VAR_P1);
+  ASSERT_EQ(Ifce.getComponentVarIndex(std::string("p2")), HvdcLineInterfaceIIDM::VAR_P2);
+  ASSERT_EQ(Ifce.getComponentVarIndex(std::string("q1")), HvdcLineInterfaceIIDM::VAR_Q1);
+  ASSERT_EQ(Ifce.getComponentVarIndex(std::string("q2")), HvdcLineInterfaceIIDM::VAR_Q2);
+  ASSERT_EQ(Ifce.getComponentVarIndex(std::string("state1")), HvdcLineInterfaceIIDM::VAR_STATE1);
+  ASSERT_EQ(Ifce.getComponentVarIndex(std::string("state2")), HvdcLineInterfaceIIDM::VAR_STATE2);
+  ASSERT_EQ(Ifce.getComponentVarIndex(std::string("invalid")), -1);
+
+  ASSERT_EQ(Ifce.getConverterMode(), HvdcLineInterface::RECTIFIER_INVERTER);
+  hvdcLine.setConvertersMode(powsybl::iidm::HvdcLine::ConvertersMode::SIDE_1_INVERTER_SIDE_2_RECTIFIER);
+  ASSERT_EQ(Ifce.getConverterMode(), HvdcLineInterface::INVERTER_RECTIFIER);
+  hvdcLine.setConvertersMode(powsybl::iidm::HvdcLine::ConvertersMode::SIDE_1_RECTIFIER_SIDE_2_INVERTER);
+  ASSERT_EQ(Ifce.getConverterMode(), HvdcLineInterface::RECTIFIER_INVERTER);
+
+  Ifce.importStaticParameters();                                           // TODO
+  ASSERT_THROW(Ifce.exportStateVariablesUnitComponent(), std::exception);  // UnaffectedStateVariable p1 HVDC1 // TODO
+}  // TEST(DataInterfaceTest, HvdcLine)
+};  // namespace DYN
