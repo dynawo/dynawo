9fbe21953ce2846e51a78ea3f1214c507c88cc5d
rosiereflo <florentine.rosiere@rte-france.com>
2020-12-01 10:09:26 +0100

    #1029 Fix deploy when libxml2 is not installed on the system

--- util/envDynawo.sh
+++ util/envDynawo.sh
@@ -1749,7 +1749,7 @@ deploy_dynawo() {
     echo "deploying libxml2"
     if [ $DYNAWO_LIBXML2_HOME_DEFAULT != true ]; then
       libxml2_system_folder=$DYNAWO_THIRD_PARTY_INSTALL_DIR
-      libxml2_system_folder_include=$DYNAWO_LIBXML2_HOME/include
+      libxml2_system_folder_include=$DYNAWO_THIRD_PARTY_INSTALL_DIR
     else
       libxml2_system_folder=$(find_lib_system_path xml2) || error_exit "Path for libxml2 could not be found for deploy."
       libxml2_system_folder_include=$(find_include_system_path LIBXML2_INCLUDE_DIR) || error_exit "Path for libxml2 include could not be found for deploy."
