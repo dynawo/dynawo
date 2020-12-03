962ea917ac08022e8b95920ce21d71049b95cd8c
Le Courtois Florent <florent.lecourtois@rte-france.com>
2020-12-03 13:43:37 +0100

    #1296 Correct cmake

--- dynawo/cmake/UnitTest.cmake
+++ dynawo/cmake/UnitTest.cmake
@@ -8,7 +8,7 @@
 #
 # This file is part of Dynawo, an hybrid C++/Modelica open source time domain simulation tool for power systems.
 
-if (NOT (CMAKE_BUILD_TYPE STREQUAL "Debug" OR CMAKE_BUILD_TYPE STREQUAL "Tests"))
+if (NOT (CMAKE_BUILD_TYPE STREQUAL "Debug" OR BUILD_TESTS))
   message(WARNING "Unit tests should be launched with debug or tests configuration")
 endif()
 
