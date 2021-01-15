ca3e1945877d256ab38984a7f1aeb88704f9e4eb
Gautier Bureau <gautier.bureau@gmail.com>
2021-01-15 17:21:44 +0100

    #408 Use lowercase in cmake instructions.

--- dynawo/cmake/FindlibZIP.cmake
+++ dynawo/cmake/FindlibZIP.cmake
@@ -16,58 +16,58 @@
 # libZIP_LIBRARY      - library to link
 # libZIP_INCLUDE_DIR  - include search path
 
-IF(NOT LIBZIP_HOME AND NOT $ENV{LIBZIP_HOME} STREQUAL "")
-  SET(LIBZIP_HOME $ENV{LIBZIP_HOME})
-ENDIF()
+if(NOT LIBZIP_HOME AND NOT $ENV{LIBZIP_HOME} STREQUAL "")
+  set(LIBZIP_HOME $ENV{LIBZIP_HOME})
+endif()
 
-IF(NOT LIBZIP_HOME AND NOT $ENV{LIBZIP_ROOT} STREQUAL "")
-  SET(LIBZIP_HOME $ENV{LIBZIP_ROOT})
-ENDIF()
+if(NOT LIBZIP_HOME AND NOT $ENV{LIBZIP_ROOT} STREQUAL "")
+  set(LIBZIP_HOME $ENV{LIBZIP_ROOT})
+endif()
 
-IF(NOT LIBZIP_HOME AND NOT $ENV{LIBZIP_INSTALL_DIR} STREQUAL "")
-  SET(LIBZIP_HOME $ENV{LIBZIP_INSTALL_DIR})
-ENDIF()
+if(NOT LIBZIP_HOME AND NOT $ENV{LIBZIP_INSTALL_DIR} STREQUAL "")
+  set(LIBZIP_HOME $ENV{LIBZIP_INSTALL_DIR})
+endif()
 
-FIND_PATH(libZIP_INCLUDE_DIR NAME "libzip/ZipVersion.h" HINTS ${LIBZIP_HOME}/include)
-FIND_LIBRARY(libZIP_LIBRARY NAME zip libzip HINTS ${LIBZIP_HOME}/lib)
+find_path(libZIP_INCLUDE_DIR NAME "libzip/ZipVersion.h" HINTS ${LIBZIP_HOME}/include)
+find_library(libZIP_LIBRARY NAME zip libzip HINTS ${LIBZIP_HOME}/lib)
 
-MARK_AS_ADVANCED(libZIP_INCLUDE_DIR libZIP_LIBRARY)
+mark_as_advanced(libZIP_INCLUDE_DIR libZIP_LIBRARY)
 
 # Extract the version number from the header.
-SET(libZIP_VERSION "")
-IF(libZIP_INCLUDE_DIR AND EXISTS "${libZIP_INCLUDE_DIR}/libzip/ZipVersion.h")
+set(libZIP_VERSION "")
+if(libZIP_INCLUDE_DIR AND EXISTS "${libZIP_INCLUDE_DIR}/libzip/ZipVersion.h")
   # The version string appears in the following in the header:
   # #define  LIBZIP_VERSION 100300
-  SET(_LIBZIP_VERSION_REGEX "^#define[ \t]+LIBZIP_VERSION[ \t]+([0-9]+)")
-  FILE(STRINGS "${libZIP_INCLUDE_DIR}/libzip/ZipVersion.h" _LIBZIP_VERSION_STRING LIMIT_COUNT 1 REGEX "${_LIBZIP_VERSION_REGEX}")
-  STRING(REGEX REPLACE ${_LIBZIP_VERSION_REGEX} "\\1" _LIBZIP_VERSION_STRING "${_LIBZIP_VERSION_STRING}")
-  IF(_LIBZIP_VERSION_STRING)
-    MATH(EXPR LIBZIP_MAJOR_VERSION "${_LIBZIP_VERSION_STRING} / 100000")
-    MATH(EXPR LIBZIP_MINOR_VERSION "${_LIBZIP_VERSION_STRING} / 100 % 1000")
-    MATH(EXPR LIBZIP_SUBMINOR_VERSION "${_LIBZIP_VERSION_STRING} % 100")
-    SET(libZIP_VERSION "${LIBZIP_MAJOR_VERSION}.${LIBZIP_MINOR_VERSION}.${LIBZIP_SUBMINOR_VERSION}")
-  ENDIF()
-ENDIF()
+  set(_LIBZIP_VERSION_REGEX "^#define[ \t]+LIBZIP_VERSION[ \t]+([0-9]+)")
+  file(STRINGS "${libZIP_INCLUDE_DIR}/libzip/ZipVersion.h" _LIBZIP_VERSION_STRING LIMIT_COUNT 1 REGEX "${_LIBZIP_VERSION_REGEX}")
+  string(REGEX REPLACE ${_LIBZIP_VERSION_REGEX} "\\1" _LIBZIP_VERSION_STRING "${_LIBZIP_VERSION_STRING}")
+  if(_LIBZIP_VERSION_STRING)
+    math(EXPR LIBZIP_MAJOR_VERSION "${_LIBZIP_VERSION_STRING} / 100000")
+    math(EXPR LIBZIP_MINOR_VERSION "${_LIBZIP_VERSION_STRING} / 100 % 1000")
+    math(EXPR LIBZIP_SUBMINOR_VERSION "${_LIBZIP_VERSION_STRING} % 100")
+    set(libZIP_VERSION "${LIBZIP_MAJOR_VERSION}.${LIBZIP_MINOR_VERSION}.${LIBZIP_SUBMINOR_VERSION}")
+  endif()
+endif()
 
-MESSAGE("-- libZIP version : ${libZIP_VERSION}")
-IF(libZIP_FIND_VERSION)
-  IF("${libZIP_VERSION}" VERSION_LESS "${libZIP_FIND_VERSION}")
-    MESSAGE("\tDetected version of libZIP is too old. Requested version was ${libZIP_FIND_VERSION}.")
-    SET(libZIP_FOUND FALSE)
-  ELSE()
-    SET(libZIP_FOUND TRUE)
-  ENDIF()
-ELSE()
+message("-- libZIP version : ${libZIP_VERSION}")
+if(libZIP_FIND_VERSION)
+  if("${libZIP_VERSION}" VERSION_LESS "${libZIP_FIND_VERSION}")
+    message("\tDetected version of libZIP is too old. Requested version was ${libZIP_FIND_VERSION}.")
+    set(libZIP_FOUND FALSE)
+  else()
+    set(libZIP_FOUND TRUE)
+  endif()
+else()
   # Handle the QUIETLY and REQUIRED arguments and set libZIP_FOUND
   # to TRUE if all listed variables are TRUE.
   # (Use ${CMAKE_ROOT}/Modules instead of ${CMAKE_CURRENT_LIST_DIR} because CMake
   #  itself includes this FindlibZIP when built with an older CMake that does
   #  not provide it.  The older CMake also does not have CMAKE_CURRENT_LIST_DIR.)
-  INCLUDE(FindPackageHandleStandardArgs)
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(libZIP
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(libZIP
     REQUIRED_VARS libZIP_LIBRARY libZIP_INCLUDE_DIR
     VERSION_VAR libZIP_VERSION)
-ENDIF()
+endif()
 
 if(libZIP_FOUND)
   set(libZIP_INCLUDE_DIRS "${libZIP_INCLUDE_DIR}")
