be9d4f804c0af17c79736db53b17e1737dcf1c7e
kouki <mohamed.kouki@isigk.rnu.tn>
2021-04-13 13:33:42 +0200

    Update CMakeLists by including ModalAnalysisTest

--- dynawo/sources/Modeler/Common/test/Test.cpp
+++ dynawo/sources/Modeler/Common/test/Test.cpp
@@ -22,6 +22,16 @@
 
 #include <boost/shared_ptr.hpp>
 #include <boost/pointer_cast.hpp>
+#include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>
+
+
+#include <boost/test/auto_unit_test.hpp>
+
+// Boost Test declaration and Checking macros
+#include <boost/test/unit_test_suite.hpp>
+#include <boost/test/test_tools.hpp>
+#include <boost/test/floating_point_comparison.hpp>
 
 #include "DYNParameterModeler.h"
 #include "gtest_dynawo.h"
@@ -43,12 +53,14 @@
 #include "PARParametersSetFactory.h"
 #include "DYNDynamicData.h"
 
+
 #include "DYNError.h"
 #include "DYNErrorQueue.h"
 #include "DYNStaticRefInterface.h"
 
 using boost::shared_ptr;
 using boost::dynamic_pointer_cast;
+
 
 using parameters::ParametersSet;
 using parameters::ParametersSetFactory;
@@ -1095,7 +1107,7 @@ TEST(ModelerCommonTest, CommonModeler) {
 }
 
 TEST(ModelerCommonTest, StaticRefInterface) {
-  StaticRefInterface sri;
+    StaticRefInterface sri;
 
   ASSERT_EQ(sri.getModelID(), "");
   ASSERT_EQ(sri.getModelVar(), "");
@@ -1109,4 +1121,5 @@ TEST(ModelerCommonTest, StaticRefInterface) {
   ASSERT_EQ(sri.getModelVar(), "MyModelVar");
   ASSERT_EQ(sri.getStaticVar(), "MyStaticVar");
 }
+
 }  // namespace DYN
