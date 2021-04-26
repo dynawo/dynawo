ff9718b72a0d95d5297eceddfb7acae237bac2e8
COSSART Quentin <cossartque@gm0winl105.bureau.si.interne>
2021-04-26 12:01:17 +0200

    #1617 Get rid of the Modelica shunt regulation for DynaFlow

--- nrt/data/DynaFlow/cases.py
+++ nrt/data/DynaFlow/cases.py
@@ -78,16 +78,6 @@ job_file = os.path.join(os.path.dirname(__file__), "Shunts", "Shunt.jobs")
 
 test_cases.append((case_name, case_description, job_file, 1, standardReturnCodeType, standardReturnCode))
 
-######################################################################
-#  Small System with a Shunt with sections and a section regulation  #
-######################################################################
-
-case_name = "DynaFlow - Shunts with regulation"
-case_description = "Small System with a Shunt with Sections and a Section Regulation"
-job_file = os.path.join(os.path.dirname(__file__), "ShuntWithRegulation", "Shunt.jobs")
-
-test_cases.append((case_name, case_description, job_file, 1, standardReturnCodeType, standardReturnCode))
-
 #######################################################################
 #  Small System with two Shunts with sections and sections regulation  #
 #######################################################################
