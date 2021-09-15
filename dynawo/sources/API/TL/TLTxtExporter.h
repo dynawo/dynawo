9bf8222f34199b3a48b454e24bb966389fcf5950
Frédéric Vallée <carving78-github@yahoo.com>
2021-09-15 15:59:46 +0200

    #1647 fix doxygen

--- dynawo/sources/API/TL/TLTxtExporter.h
+++ dynawo/sources/API/TL/TLTxtExporter.h
@@ -42,6 +42,7 @@ class TxtExporter : public Exporter {
    *
    * @param timeline Timeline to export
    * @param filePath File to export txt formatted timeline to
+   * @param exportWithTime whether to export time
    */
   void exportToFile(const boost::shared_ptr<Timeline>& timeline, const std::string& filePath,
                     const bool exportWithTime) const;
@@ -51,6 +52,7 @@ class TxtExporter : public Exporter {
    *
    * @param timeline Timeline to export
    * @param stream stream to export txt formatted timeline to
+   * @param exportWithTime whether to export time
    */
   void exportToStream(const boost::shared_ptr<Timeline>& timeline, std::ostream& stream,
                       const bool exportWithTime) const;
