9f3777924d5e8ae50d196435c031955f4877722c
Le Courtois Florent <florent.lecourtois@rte-france.com>
2021-12-06 08:57:23 +0100

    #1974 Fix solver implementation

--- dynawo/sources/Solvers/FixedTimeStep/DYNSolverCommonFixedTimeStep.h
+++ dynawo/sources/Solvers/FixedTimeStep/DYNSolverCommonFixedTimeStep.h
@@ -73,8 +73,9 @@ class SolverCommonFixedTimeStep : public Solver::Impl {
    *
    * @param counter of initialization calculation
    * @param change check if there is a z or a mode change.
+   * @returns true if we must continue the algorithm, false if we must retry
    */
-  void calculateICCommonModeChange(int& counter, bool& change);
+  bool calculateICCommonModeChange(int& counter, bool& change);
 
   /**
   * @brief compute YP prediction
