3f9cdf8ee27174ef81e89d05655e0bad25a78ddb
rosiereflo <florentine.rosiere@rte-france.com>
2022-03-11 12:50:20 +0100

    #2106 add a tolerance to compare final state values in nrt closes #2106

---
+++ util/nrt_diff/finalStateValuesDiff.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2022, RTE (http://www.rte-france.com)
+# See AUTHORS.txt
+# All rights reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+#
+# This file is part of Dynawo, an hybrid C++/Modelica open source time domain
+# simulation tool for power systems.
+
+import operator
+import os
+import sys
+import XMLUtils
+
+try:
+    settings_dir = os.path.join(os.path.dirname(__file__))
+    sys.path.append(settings_dir)
+    import settings
+except Exception as exc:
+    print("Failed to import nrtDiff settings : " + str(exc))
+    sys.exit(1)
+
+# Utility class to compare Constraints files
+class FinalStateValueObject:
+    def __init__(self):
+        self.model_name=""
+        self.variable=""
+        self.value = ""
+
+    def get_unique_id(self):
+        return self.model_name+"_"+self.variable
+
+# Read a XML FSV file name and build a dictionary object id => values
+def get_xml_fsv_info(filename):
+    fsv_by_id = {}
+    (fsv_root, ns, prefix) = XMLUtils.ImportXMLFileExtended(filename)
+    for child in XMLUtils.FindAll(fsv_root, prefix, "finalStateValue", ns):
+        my_object = FinalStateValueObject();
+        my_object.model_name = child.attrib['model']
+        my_object.variable = child.attrib['variable']
+        my_object.value = child.attrib['value']
+        fsv_by_id[my_object.get_unique_id()] = my_object
+    return fsv_by_id
+
+# Compare 2 dictionaries read from two fsv files
+# @param left_file_info : the dictionary from the left path
+# @param right_file_info : the dictionary from the right path
+def compare_fsv_info (left_file_info, right_file_info):
+    nb_differences = 0
+    msg = ""
+    differences = []
+
+    for firstId in sorted(left_file_info):
+        if firstId not in right_file_info:
+            nb_differences+=1
+            msg += "[ERROR] object " + firstId + " is in left path but not in right one\n"
+    for firstId in sorted(right_file_info):
+        if firstId not in left_file_info:
+            nb_differences+=1
+            msg += "[ERROR] object " + firstId + " is in right path but not in left one\n"
+    for firstId in sorted(left_file_info):
+        firstObj = left_file_info[firstId]
+        if firstId in sorted(right_file_info):
+            secondObj = right_file_info[firstId]
+            if firstObj.value != "" or secondObj.value != "":
+                try:
+                    difference = abs(float(firstObj.value)- float(secondObj.value))
+                    if (difference > settings.max_iidm_cmp_tol):
+                        nb_differences+=1
+                        differences.append([difference, firstId])
+                except ValueError:
+                    nb_differences+=1
+                    msg += "[ERROR] object " + firstId + " has different values in the two files\n"
+
+    for error in sorted(differences, key=operator.itemgetter(0), reverse=True)[:settings.max_nb_iidm_outputs]:
+        msg += "[ERROR] values of object " +  error[1] + " are different (delta = " + str(error[0]) + ") \n"
+    return (nb_differences, msg)
+
+# Check whether two xml output fsv files are close enough
+# @param path_left : the absolute path to the left-side file
+# @param path_right : the absolute path to the right-side file
+def output_xml_fsv_close_enough (path_left, path_right):
+    left_file_info = get_xml_fsv_info(path_left)
+    right_file_info = get_xml_fsv_info(path_right)
+    return compare_fsv_info(left_file_info, right_file_info)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Error : not enough arguments")
+    path_left = sys.argv[1]
+    path_right = sys.argv[2]
+    print("Comparing " + path_left + " and " + path_right)
+    nb_differences, msg = output_xml_fsv_close_enough(path_left, path_right)
+    if nb_differences > 0:
+        print(msg)
+        exit(1)
+    print("OK")
