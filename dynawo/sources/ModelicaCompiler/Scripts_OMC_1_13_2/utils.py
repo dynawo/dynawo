10393f608db233b3f0d9b3d9c2541c0db43bbab5
rosiereflo <florentine.rosiere@rte-france.com>
2022-07-13 10:47:11 +0200

    #2428 fix transcription of state variables for evalFAdept closes #2428

--- dynawo/sources/ModelicaCompiler/Scripts_OMC_1_13_2/utils.py
+++ dynawo/sources/ModelicaCompiler/Scripts_OMC_1_13_2/utils.py
@@ -521,7 +521,7 @@ class Transpose:
     # @param residual_vars_map : residual vars
     def __init__(self, auxiliary_vars_map = None, residual_vars_map = None):
         ## pattern to intercept var name in expression
-        self.ptrn_vars = re.compile(r'data->localData\[[0-9]+\]->derivativesVars\[[0-9]+\][ ]+\/\*[ \w\$\.()\[\]]*\*\/|data->localData\[[0-9]+\]->realVars\[[0-9]+\][ ]+\/\*[ \w\$\.()\[\]]*[ ]variable[ ]\*\/|data->localData\[[0-9]+\]->realVars\[[0-9]+\][ ]+\/\*[ \w\$\.()\[\]]*[ ]*\*\/')
+        self.ptrn_vars = re.compile(r'data->localData\[[0-9]+\]->derivativesVars\[[0-9]+\][ ]+\/\*[ \w\$\.()\[\],]*\*\/|data->localData\[[0-9]+\]->realVars\[[0-9]+\][ ]+\/\*[ \w\$\.()\[\],]*[ ]variable[ ]\*\/|data->localData\[[0-9]+\]->realVars\[[0-9]+\][ ]+\/\*[ \w\$\.()\[\],]*[ ]*\*\/')
         ## map associating var name to var value
         self.map = {}
         ## expressions where var name should be replaced
@@ -554,7 +554,7 @@ class Transpose:
                 if 'derivativesVars' not in name:
                     continue
                 # If the var "name" is in the map, we replace it by its other expression (xd[...])
-                ptrn_real_var = re.compile(r'data->localData\[[0-9]+\]->derivativesVars\[(?P<varId>[0-9]+)\][ ]+\/\*[ \w\$\.()\[\]]*\*\/')
+                ptrn_real_var = re.compile(r'data->localData\[[0-9]+\]->derivativesVars\[(?P<varId>[0-9]+)\][ ]+\/\*[ \w\$\.()\[\],]*\*\/')
                 match = ptrn_real_var.search(name)
                 if match is not None:
                     if "= modelica_real_to_modelica_string(" in line_tmp:
@@ -565,7 +565,7 @@ class Transpose:
                 if 'derivativesVars' in name:
                     continue
                 # If the var "name" is in the map, we replace it by its other expression (x[...])
-                ptrn_real_var = re.compile(r'data->localData\[[0-9]+\]->realVars\[(?P<varId>[0-9]+)\][ ]+\/\*[ \w\$\.()\[\]]*\*\/')
+                ptrn_real_var = re.compile(r'data->localData\[[0-9]+\]->realVars\[(?P<varId>[0-9]+)\][ ]+\/\*[ \w\$\.()\[\],]*\*\/')
                 match = ptrn_real_var.search(name)
                 if match is not None:
                     if "= modelica_real_to_modelica_string(" in line_tmp:
@@ -579,6 +579,8 @@ class Transpose:
                     line_tmp = line_tmp.replace("(modelica_boolean)"+name, "(modelica_boolean)( ("+name+">0)? 1: 0 )")
             for name in self.residual_vars_map:
                 line_tmp = line_tmp.replace("$P"+name, name)
+            assert ("derivativesVars[" not in line_tmp)
+            assert ("realVars[" not in line_tmp)
             tmp_txt_list.append(line_tmp)
         return tmp_txt_list
 
