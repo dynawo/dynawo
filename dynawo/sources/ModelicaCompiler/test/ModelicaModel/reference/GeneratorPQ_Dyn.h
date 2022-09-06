4521be7f2a70dc678bd54c080ee161608880ee67
Dimitri Baron <dimitri.baron@rte-france.com>
2022-09-06 16:19:28 +0200

    #2385 Optimization of defineParameters and defineElements functions

--- dynawo/sources/ModelicaCompiler/test/ModelicaModel/reference/GeneratorPQ_Dyn.h
+++ dynawo/sources/ModelicaCompiler/test/ModelicaModel/reference/GeneratorPQ_Dyn.h
@@ -62,7 +62,7 @@ namespace DYN {
     inline void setModelType(std::string modelType) { modelType_ = modelType; }
     inline ModelManager * getModelManager() const { return modelManager_; }
     inline void setModelManager (ModelManager * model) { modelManager_ = model; }
-    void checkSum(std::string & checkSum) { checkSum = std::string("5ca515d4216cb748c52afb0769f16336"); }
+    void checkSum(std::string & checkSum) { checkSum = std::string("ac0d957dedbae7fcfbbd960c1f3d1648"); }
     inline bool isDataStructInitialized() const { return dataStructInitialized_; }
 
     private:
