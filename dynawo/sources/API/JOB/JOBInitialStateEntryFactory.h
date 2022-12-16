0185d2ee2524733156b7c2f390cc25bf687c5223
rosiereflo <florentine.rosiere@rte-france.com>
2022-12-16 09:30:52 +0100

    #2690 Add initial state entry factory in jobs closes #2690

---
+++ dynawo/sources/API/JOB/JOBInitialStateEntryFactory.h
@@ -0,0 +1,46 @@
+//
+// Copyright (c) 2022, RTE (http://www.rte-france.com)
+// See AUTHORS.txt
+// All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, you can obtain one at http://mozilla.org/MPL/2.0/.
+// SPDX-License-Identifier: MPL-2.0
+//
+// This file is part of Dynawo, an hybrid C++/Modelica open source time domain
+// simulation tool for power systems.
+//
+
+/**
+ * @file  JOBInitialStateEntryFactory.h
+ *
+ * @brief Dynawo initial state entry factory : header file
+ *
+ */
+#ifndef API_JOB_JOBINITIALSTATEENTRYFACTORY_H_
+#define API_JOB_JOBINITIALSTATEENTRYFACTORY_H_
+
+#include "JOBInitialStateEntry.h"
+
+#include <boost/shared_ptr.hpp>
+namespace job {
+
+/**
+ * @class  InitialStateEntryFactory
+ *
+ * @brief initial state entry factory
+ *
+ */
+class InitialStateEntryFactory {
+ public:
+  /**
+   * @brief create a new instance of initial state entry
+   *
+   * @return dynamic model entry
+   */
+  static boost::shared_ptr<InitialStateEntry> newInstance();
+};
+
+}  // namespace job
+
+#endif  // API_JOB_JOBINITIALSTATEENTRYFACTORY_H_
