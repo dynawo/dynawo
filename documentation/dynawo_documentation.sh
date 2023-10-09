7873d7c03a678be620e707db85b83657b37ce4f0
Florentine Rosiere <florentine.rosiere@rte-france.com>
2023-10-09 10:36:30 +0200

    #3051 v1.5.1
    Signed-off-by: Florentine Rosiere <florentine.rosiere@rte-france.com>

--- documentation/dynawo_documentation.sh
+++ documentation/dynawo_documentation.sh
@@ -28,7 +28,7 @@ echo '%% Except where otherwise noted, content in this documentation is Copyrigh
 
 \begin{document}
 
-\title{\huge{\Dynawo Documentation} \\ \LARGE{v1.5.0} \\
+\title{\huge{\Dynawo Documentation} \\ \LARGE{v1.5.1} \\
 \vspace{10mm}
 \includegraphics[width=0.7\textwidth]{../resources/Dynawo-Logo-Color.png}}
 \date\today
