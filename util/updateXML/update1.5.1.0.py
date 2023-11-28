ebc74f3eade6e5f95924e77d1f5abcff61807b76
Florentine Rosiere <florentine.rosiere@rte-france.com>
2023-11-28 13:24:39 +0100

    #1581 first update scripts for v1.5.1 and v1.6.0
    Signed-off-by: Florentine Rosiere <florentine.rosiere@rte-france.com>

---
+++ util/updateXML/update1.5.1.0.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2023, RTE (http://www.rte-france.com)
+# See AUTHORS.txt
+# All rights reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+#
+# This file is part of Dynawo, an hybrid C++/Modelica open source time domain
+# simulation tool for power systems.
+from content.Ticket import ticket
+
+# add generator_UseApproximation parameter
+@ticket(2818)
+def update(job):
+    gens = job.dyds.get_bbms(lambda bbm: bbm.get_lib_name().startswith("Generator"))
+    for gen in gens:
+        gen.parset.add_param("BOOL", "generator_UseApproximation", True)
