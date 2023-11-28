abc1501a8358585ea2d74b9d497bb47be6028741
Dimitri Baron <dimitri.baron@rte-france.com>
2023-11-28 15:25:22 +0100

    #1581 Put the Dynawo licence in every files
    Signed-off-by: Dimitri Baron <dimitri.baron@rte-france.com>

--- util/updateXML/content/test/TestDyd/TestModelTemplateExpansion/res/change_static_ref_var_name/change_static_ref_var_name1.4.0.0.py
+++ util/updateXML/content/test/TestDyd/TestModelTemplateExpansion/res/change_static_ref_var_name/change_static_ref_var_name1.4.0.0.py
@@ -1,3 +1,15 @@
+# Copyright (c) 2023, RTE (http://www.rte-france.com)
+# See AUTHORS.txt
+# All rights reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+#
+# This file is part of Dynawo, an hybrid C++/Modelica open source suite
+# of simulation tools for power systems.
+
+
 def update(jobs):
     gens = jobs.dyds.get_model_template_expansions(
         lambda model_template_expansion: model_template_expansion.get_template_id() == "MachineThreeWindingsTemplate"
--- util/updateXML/content/test/TestDyd/TestModelicaModel/res/change_unit_dynamic_model_name/change_unit_dynamic_model_name1.4.0.0.py
+++ util/updateXML/content/test/TestDyd/TestModelicaModel/res/change_unit_dynamic_model_name/change_unit_dynamic_model_name1.4.0.0.py
@@ -1,3 +1,15 @@
+# Copyright (c) 2023, RTE (http://www.rte-france.com)
+# See AUTHORS.txt
+# All rights reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+#
+# This file is part of Dynawo, an hybrid C++/Modelica open source suite
+# of simulation tools for power systems.
+
+
 def update(jobs):
     modelica_models = jobs.dyds.get_modelica_models(lambda modelica_model: modelica_model.get_id() == "GEN____1_SM")
     for modelica_model in modelica_models:
