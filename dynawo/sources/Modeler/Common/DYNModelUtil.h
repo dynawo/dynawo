9e1466e849011a28d22bb539b81134b261af8b3b
Gautier Bureau <gautier.bureau@gmail.com>
2024-05-16 16:43:31 +0200

    #3059 Small fixes for code quality 2.
    Signed-off-by: Gautier Bureau <gautier.bureau@gmail.com>

--- dynawo/sources/Modeler/Common/DYNModelUtil.h
+++ dynawo/sources/Modeler/Common/DYNModelUtil.h
@@ -29,10 +29,14 @@ namespace DYN {
    *
    * This function is for debug purposes but can be used release mode
    *
+   * To use it you need to add #include "DYNModelUtil.h" in DYNSolverIDA.cpp
+   * and add dynawo_ModelerCommon to the target_link_libraries of dynawo_SolverIDA.
+   * Then in evalJ you can use printStructureToFile(model, smj);
+   *
    * @param model model to get the informations on equations and variables
    * @param matrix matrix which structure will be printed
    */
-  void printStructureToFile(const boost::shared_ptr<Model>& model, const SparseMatrix& matrix);
+  void printStructureToFile(Model& model, const SparseMatrix& matrix);
 }  // namespace DYN
 
 #endif  // MODELER_COMMON_DYNMODELUTIL_H_
