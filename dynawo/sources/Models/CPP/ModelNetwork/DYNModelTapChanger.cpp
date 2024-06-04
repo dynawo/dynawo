d0a3c0ebe45e51f864f5ca88706f060caf7baea3
Dimitri Baron <dimitri.baron@rte-france.com>
2024-06-04 14:39:26 +0200

    #3249 PR remark
    Signed-off-by: Dimitri Baron <dimitri.baron@rte-france.com>

--- dynawo/sources/Models/CPP/ModelNetwork/DYNModelTapChanger.cpp
+++ dynawo/sources/Models/CPP/ModelNetwork/DYNModelTapChanger.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2023, RTE (http://www.rte-france.com)
+// Copyright (c) 2024, RTE (http://www.rte-france.com)
 // See AUTHORS.txt
 // All rights reserved.
 // This Source Code Form is subject to the terms of the Mozilla Public
