e30fd279ca24ec961989bded68c7d9d7275911a5
Erwann GOASGUEN <erwann.goasguen_externe@rte-france.com>
2025-04-03 11:32:20 +0200

    #3615 Check if double less than another
    Signed-off-by: Erwann GOASGUEN <erwann.goasguen_externe@rte-france.com>

--- dynawo/sources/Common/DYNCommon.h
+++ dynawo/sources/Common/DYNCommon.h
@@ -167,6 +167,18 @@ namespace DYN {
   }
 
   /**
+   * @brief return true if a < b
+   *
+   *
+   * @param a first double
+   * @param b second double
+   * @return return true if a < b
+   */
+  static inline bool doubleLess(double a, double b) {
+    return doubleNotEquals(a, b) && a < b;
+  }
+
+  /**
    * @brief return true if a == 0.
    *
    *
