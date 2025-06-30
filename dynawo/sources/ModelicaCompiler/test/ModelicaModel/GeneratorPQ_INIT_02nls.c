a019ac602eb2345627da5cb28f0029ee9049b798
Florentine Rosiere <florentine.rosiere@rte-france.com>
2025-06-30 14:23:28 +0200

    #2292 Fix unit tests
    Signed-off-by: Florentine Rosiere <florentine.rosiere@rte-france.com>

--- dynawo/sources/ModelicaCompiler/test/ModelicaModel/GeneratorPQ_INIT_02nls.c
+++ dynawo/sources/ModelicaCompiler/test/ModelicaModel/GeneratorPQ_INIT_02nls.c
@@ -2,6 +2,7 @@
 #include "GeneratorPQ_INIT_model.h"
 #include "GeneratorPQ_INIT_16dae.h"
 #include "GeneratorPQ_INIT_12jac.h"
+#include "simulation/jacobian_util.h"
 #if defined(__cplusplus)
 extern "C" {
 #endif
