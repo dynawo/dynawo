ec33b13b88ccd1012471a177b4d3c39207a32cc8
Loïc Bartel <loic.bartel_externe@rte-france.com>
2025-07-03 15:17:50 +0200

    #3593 Fix indentation, refactoring
    Signed-off-by: Loïc Bartel <loic.bartel_externe@rte-france.com>

--- util/updateXML/update1.8.0.4.py
+++ util/updateXML/update1.8.0.4.py
@@ -16,50 +16,21 @@ from content.Ticket import ticket
 # add former default parameter values to .par
 @ticket(3593)
 def update(jobs):
-    modelica_models = jobs.dyds.get_modelica_models(lambda _: True)
-        for modelica_model in modelica_models:
-            unit_dynamic_models = modelica_model.get_unit_dynamic_models(
-                lambda unit_dynamic_model: unit_dynamic_model.get_name() == "Dynawo.Electrical.Wind.WECC.WTG4ACurrentSource"
-            )
-            for unit_dynamic_model in unit_dynamic_models:
-                unit_dynamic_model.set_name("Dynawo.Electrical.Wind.WECC.WTG4ACurrentSource1")
-    modelica_models = jobs.dyds.get_modelica_models(lambda _: True)
-        for modelica_model in modelica_models:
-            unit_dynamic_models = modelica_model.get_unit_dynamic_models(
-                lambda unit_dynamic_model: unit_dynamic_model.get_name() == "Dynawo.Electrical.Controls.WECC.Mechanical.DriveTrainPmConstant"
-            )
-            for unit_dynamic_model in unit_dynamic_models:
-            unit_dynamic_model.set_name("Dynawo.Electrical.Controls.WECC.Mechanical.WTGTa")
-    modelica_models = jobs.dyds.get_modelica_models(lambda _: True)
-        for modelica_model in modelica_models:
-            unit_dynamic_models = modelica_model.get_unit_dynamic_models(
-                lambda unit_dynamic_model: unit_dynamic_model.get_name() == "Dynawo.Electrical.Controls.WECC.Mechanical.DriveTrainPeFiltered"
-            )
-            for unit_dynamic_model in unit_dynamic_models:
-            unit_dynamic_model.set_name("Dynawo.Electrical.Controls.WECC.Mechanical.WTGTb")
+    for mm in jobs.dyds.get_modelica_models(lambda _: True):
+        update_names(mm.get_unit_dynamic_models(lambda _: True))
 
-    model_templates = jobs.dyds.get_model_templates(lambda _: True)
-        for model_template in model_templates:
-            unit_dynamic_models = model_template.get_unit_dynamic_models(
-                lambda unit_dynamic_model: unit_dynamic_model.get_name() == "Dynawo.Electrical.Wind.WECC.WTG4ACurrentSource"
-            )
-            for unit_dynamic_model in unit_dynamic_models:
-                unit_dynamic_model.set_name("Dynawo.Electrical.Wind.WECC.WTG4ACurrentSource1")
-    model_templates = jobs.dyds.get_model_templates(lambda _: True)
-        for model_template in model_templates:
-            unit_dynamic_models = model_template.get_unit_dynamic_models(
-                lambda unit_dynamic_model: unit_dynamic_model.get_name() == "Dynawo.Electrical.Controls.WECC.Mechanical.DriveTrainPmConstant"
-            )
-            for unit_dynamic_model in unit_dynamic_models:
-            unit_dynamic_model.set_name("Dynawo.Electrical.Controls.WECC.Mechanical.WTGTa")
-    model_templates = jobs.dyds.get_model_templates(lambda _: True)
-        for model_template in model_templates:
-            unit_dynamic_models = model_template.get_unit_dynamic_models(
-                lambda unit_dynamic_model: unit_dynamic_model.get_name() == "Dynawo.Electrical.Controls.WECC.Mechanical.DriveTrainPeFiltered"
-            )
-            for unit_dynamic_model in unit_dynamic_models:
-            unit_dynamic_model.set_name("Dynawo.Electrical.Controls.WECC.Mechanical.WTGTb")
+    for mt in jobs.dyds.get_model_templates(lambda _: True):
+        update_names(mt.get_unit_dynamic_models(lambda _: True))
 
-    windxmls = jobs.dyds.get_bbms(lambda bbm: bbm.get_lib_name() == "WTG4AWeccCurrentSource")
-    for windxml in windxmls:
+    for windxml in jobs.dyds.get_bbms(lambda bbm: bbm.get_lib_name() == "WTG4AWeccCurrentSource"):
         windxml.set_lib_name("WTG4AWeccCurrentSource1")
+
+def update_names(udms):
+    for udm in udms:
+        update_name(udm,"Dynawo.Electrical.Wind.WECC","WTG4ACurrentSource","WTG4ACurrentSource1")
+        update_name(udm,"Dynawo.Electrical.Controls.WECC.Mechanical","DriveTrainPmConstant","WTGTa")
+        update_name(udm,"Dynawo.Electrical.Controls.WECC.Mechanical","DriveTrainPeFiltered","WTGTb")
+
+def update_name(udm,prefix,old,new):
+    if udm.get_name() == prefix+"."+old:
+        udm.set_name(prefix+"."+new)
