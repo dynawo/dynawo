5fd0796b8d22d4c7e1fffbb013309a031be75a2d
Thibaut VERMEULEN <thibaut.vermeulen@rte-france.com>
2025-09-19 20:06:11 +0200

    #3692 add tests for RT jobs API
    Signed-off-by: Thibaut VERMEULEN <thibaut.vermeulen@rte-france.com>

---
+++ dynawo/sources/API/JOB/test/TestInteractiveSettingsEntry.cpp
@@ -0,0 +1,62 @@
+//
+// Copyright (c) 2015-2019, RTE (http://www.rte-france.com)
+// See AUTHORS.txt
+// All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, you can obtain one at http://mozilla.org/MPL/2.0/.
+// SPDX-License-Identifier: MPL-2.0
+//
+// This file is part of Dynawo, an hybrid C++/Modelica open source time domain
+// simulation tool for power systems.
+//
+
+/**
+ * @file API/JOB/test/TestModelerEntry.cpp
+ * @brief Unit tests for API_JOB/JOBModelerEntry class
+ *
+ */
+
+#include "gtest_dynawo.h"
+#include "JOBInteractiveSettingsEntry.h"
+#include "JOBChannelsEntry.h"
+#include "JOBClockEntry.h"
+#include "JOBStreamsEntry.h"
+#include "DYNClone.hpp"
+
+namespace job {
+
+TEST(APIJOBTest, testInteractiveSetingsEntry) {
+  std::shared_ptr<InteractiveSettingsEntry> interactiveSettings = std::make_shared<InteractiveSettingsEntry>();
+
+  // check default attributes
+  ASSERT_EQ(interactiveSettings->getCouplingTimeStep(), 1.);
+  ASSERT_EQ(interactiveSettings->getChannelsEntry(), std::shared_ptr<ChannelsEntry>());
+  ASSERT_EQ(interactiveSettings->getClockEntry(), std::shared_ptr<ClockEntry>());
+  ASSERT_EQ(interactiveSettings->getStreamsEntry(), std::shared_ptr<StreamsEntry>());
+
+  interactiveSettings->setCouplingTimeStep(2.);
+
+  std::shared_ptr<ChannelsEntry> channels = std::make_shared<ChannelsEntry>();
+  interactiveSettings->setChannelsEntry(channels);
+
+  std::shared_ptr<ClockEntry> clock = std::make_shared<ClockEntry>();
+  interactiveSettings->setClockEntry(clock);
+
+  std::shared_ptr<StreamsEntry> streams = std::make_shared<StreamsEntry>();
+  interactiveSettings->setStreamsEntry(streams);
+
+
+  ASSERT_EQ(interactiveSettings->getCouplingTimeStep(), 2.);
+  ASSERT_EQ(interactiveSettings->getChannelsEntry(), channels);
+  ASSERT_EQ(interactiveSettings->getClockEntry(), clock);
+  ASSERT_EQ(interactiveSettings->getStreamsEntry(), streams);
+
+  std::shared_ptr<InteractiveSettingsEntry> interactiveSettings_bis = DYN::clone(interactiveSettings);
+  ASSERT_EQ(interactiveSettings_bis->getCouplingTimeStep(), 2.);
+  ASSERT_EQ(interactiveSettings_bis->getChannelsEntry(), channels);
+  ASSERT_EQ(interactiveSettings_bis->getClockEntry(), clock);
+  ASSERT_EQ(interactiveSettings_bis->getStreamsEntry(), streams);
+}
+
+}  // namespace job
