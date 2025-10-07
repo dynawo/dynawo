2620bf7d7e62e10e369127eed88473d2c3d12ec6
Gautier Bureau <gautier.bureau@gmail.com>
2025-10-07 15:06:35 +0200

    #3751 Add new strategy in kinsol in case of failure.
    Signed-off-by: Gautier Bureau <gautier.bureau@gmail.com>
    
    #3751 Corrections after review.
    
    Signed-off-by: Gautier Bureau <gautier.bureau@gmail.com>
    
    #3751 Fix for residual restart.
    
    Signed-off-by: Gautier Bureau <gautier.bureau@gmail.com>

--- dynawo/sources/Solvers/AlgebraicSolvers/DYNSolverKINAlgRestoration.cpp
+++ dynawo/sources/Solvers/AlgebraicSolvers/DYNSolverKINAlgRestoration.cpp
@@ -27,6 +27,9 @@
 #include <cmath>
 
 #include "DYNSolverKINAlgRestoration.h"
+
+#include <DYNTimer.h>
+
 #include "DYNModel.h"
 #include "DYNSolverCommon.h"
 #include "DYNSparseMatrix.h"
@@ -51,9 +54,10 @@ SolverKINAlgRestoration::stringFromMode(const modeKin_t mode) {
   }
 }
 
-SolverKINAlgRestoration::SolverKINAlgRestoration() :
+SolverKINAlgRestoration::SolverKINAlgRestoration(const bool printReinitResiduals) :
 SolverKINCommon(),
-mode_(KIN_ALGEBRAIC) {
+mode_(KIN_ALGEBRAIC),
+printReinitResiduals_(printReinitResiduals) {
 #if _DEBUG_
   checkJacobian_ = false;
 #endif
@@ -232,6 +236,9 @@ SolverKINAlgRestoration::updateKINSOLSettings(const double fnormtol, const doubl
 
 int
 SolverKINAlgRestoration::evalF_KIN(N_Vector yy, N_Vector rr, void *data) {
+#if defined(_DEBUG_) || defined(PRINT_TIMERS)
+  Timer timer("SolverKINAlgRestoration::evalF_KIN");
+#endif
   SolverKINAlgRestoration* solver = reinterpret_cast<SolverKINAlgRestoration*>(data);
   Model& model = solver->getModel();
 
@@ -271,21 +278,21 @@ SolverKINAlgRestoration::evalF_KIN(N_Vector yy, N_Vector rr, void *data) {
     irr[i] = solver->vectorF_[solver->indexF_[i]];
   }
 
-#ifdef _DEBUG_
-  // Print the current residual norms, the first one is used as a stopping criterion
-  double weightedInfNorm = SolverCommon::weightedInfinityNorm(solver->vectorF_, solver->indexF_, solver->vectorFScale_);
-  double wL2Norm = SolverCommon::weightedL2Norm(solver->vectorF_, solver->indexF_, solver->vectorFScale_);
-  long int current_nni = 0;
-  KINGetNumNonlinSolvIters(solver->KINMem_, &current_nni);
-  Trace::debug() << DYNLog(SolverKINResidualNormAlg, stringFromMode(solver->getMode()), current_nni, weightedInfNorm, wL2Norm) << Trace::endline;
+  if (solver->printResiduals()) {
+    // Print the current residual norms, the first one is used as a stopping criterion
+    double weightedInfNorm = SolverCommon::weightedInfinityNorm(solver->vectorF_, solver->indexF_, solver->vectorFScale_);
+    double wL2Norm = SolverCommon::weightedL2Norm(solver->vectorF_, solver->indexF_, solver->vectorFScale_);
+    long int current_nni = 0;
+    KINGetNumNonlinSolvIters(solver->KINMem_, &current_nni);
+    Trace::debug() << DYNLog(SolverKINResidualNormAlg, stringFromMode(solver->getMode()), current_nni, weightedInfNorm, wL2Norm) << Trace::endline;
 
-  const int nbErr = 10;
-  Trace::debug() << DYNLog(KinLargestErrors, nbErr) << Trace::endline;
-  vector<std::pair<double, size_t> > fErr;
-  for (size_t i = 0; i < solver->indexF_.size(); ++i)
-    fErr.push_back(std::pair<double, size_t>(solver->vectorF_[solver->indexF_[i]], solver->indexF_[i]));
-  SolverCommon::printLargestErrors(fErr, model, nbErr);
-#endif
+    const int nbErr = 10;
+    Trace::debug() << DYNLog(KinLargestErrors, nbErr) << Trace::endline;
+    vector<std::pair<double, size_t> > fErr;
+    for (size_t i = 0; i < solver->indexF_.size(); ++i)
+      fErr.push_back(std::pair<double, size_t>(solver->vectorF_[solver->indexF_[i]], solver->indexF_[i]));
+    SolverCommon::printLargestErrors(fErr, model, nbErr);
+  }
   return 0;
 }
 
@@ -313,6 +320,9 @@ SolverKINAlgRestoration::checkJacobian(const SparseMatrix& smj, Model& model) {
 int
 SolverKINAlgRestoration::evalJ_KIN(N_Vector /*yy*/, N_Vector /*rr*/,
          SUNMatrix JJ, void* data, N_Vector /*tmp1*/, N_Vector /*tmp2*/) {
+#if defined(_DEBUG_) || defined(PRINT_TIMERS)
+  Timer timer("SolverKINAlgRestoration::evalJ_KIN");
+#endif
   SolverKINAlgRestoration* solver = reinterpret_cast<SolverKINAlgRestoration*> (data);
   Model& model = solver->getModel();
 
@@ -339,6 +349,9 @@ SolverKINAlgRestoration::evalJ_KIN(N_Vector /*yy*/, N_Vector /*rr*/,
 int
 SolverKINAlgRestoration::evalJPrim_KIN(N_Vector /*yy*/, N_Vector /*rr*/,
         SUNMatrix JJ, void* data, N_Vector /*tmp1*/, N_Vector /*tmp2*/) {
+#if defined(_DEBUG_) || defined(PRINT_TIMERS)
+  Timer timer("SolverKINAlgRestoration::evalJPrim_KIN");
+#endif
   SolverKINAlgRestoration* solver = reinterpret_cast<SolverKINAlgRestoration*> (data);
   Model& model = solver->getModel();
 
@@ -358,11 +371,23 @@ SolverKINAlgRestoration::evalJPrim_KIN(N_Vector /*yy*/, N_Vector /*rr*/,
   return 0;
 }
 
-int
-SolverKINAlgRestoration::solve(const bool noInitSetup, const bool evaluateOnlyModeAtFirstIter) {
-  if (numF_ == 0)
-    return KIN_SUCCESS;
+void SolverKINAlgRestoration::saveState() {
+  vectorYForRestorationSave_.assign(vectorYForRestoration_.begin(), vectorYForRestoration_.end());
+  vectorYpForRestorationSave_.assign(vectorYpForRestoration_.begin(), vectorYpForRestoration_.end());
+  vectorYOrYpSolutionSave_.assign(vectorYOrYpSolution_.begin(), vectorYOrYpSolution_.end());
+  // We have to save the residual because of the option evaluateOnlyModeAtFirstIter to have a proper restart
+  model_->saveResidual(vectorFSave_);
+}
 
+void SolverKINAlgRestoration::restoreState() {
+  vectorYForRestoration_.assign(vectorYForRestorationSave_.begin(), vectorYForRestorationSave_.end());
+  vectorYpForRestoration_.assign(vectorYpForRestorationSave_.begin(), vectorYpForRestorationSave_.end());
+  vectorYOrYpSolution_.assign(vectorYOrYpSolutionSave_.begin(), vectorYOrYpSolutionSave_.end());
+  model_->restoreResidual(vectorFSave_);
+}
+
+int SolverKINAlgRestoration::solveStrategy(const bool noInitSetup, const bool evaluateOnlyModeAtFirstIter, const int kinsolStategy,
+  const bool multipleStrategiesForAlgebraicRestoration) {
   int flag = KINSetNoInitSetup(KINMem_, noInitSetup);
   if (flag < 0)
     throw DYNError(Error::SUNDIALS_ERROR, SolverFuncErrorKINSOL, "KINSetNoInitSetup");
@@ -373,6 +398,10 @@ SolverKINAlgRestoration::solve(const bool noInitSetup, const bool evaluateOnlyMo
     model_->evalFMode(t0_, &vectorYForRestoration_[0], &vectorYpForRestoration_[0], &vectorF_[0]);
   else
     model_->evalF(t0_, &vectorYForRestoration_[0], &vectorYpForRestoration_[0], &vectorF_[0]);
+
+  if (multipleStrategiesForAlgebraicRestoration) {
+    vectorFSave_.assign(vectorF_.begin(), vectorF_.end());
+  }
 
   // fScale
   vectorFScale_.assign(indexF_.size(), 1.);
@@ -389,7 +418,28 @@ SolverKINAlgRestoration::solve(const bool noInitSetup, const bool evaluateOnlyMo
     }
   }
 
-  flag = solveCommon();
+  flag = solveCommon(kinsolStategy);
+
+  return flag;
+}
+
+int
+SolverKINAlgRestoration::solve(const bool noInitSetup, const bool evaluateOnlyModeAtFirstIter, const bool multipleStrategiesForAlgebraicRestoration) {
+  if (numF_ == 0)
+    return KIN_SUCCESS;
+
+  if (multipleStrategiesForAlgebraicRestoration)
+    saveState();
+
+  int flag = solveStrategy(noInitSetup, evaluateOnlyModeAtFirstIter, KIN_NONE, multipleStrategiesForAlgebraicRestoration);
+
+  if (flag  < 0 && multipleStrategiesForAlgebraicRestoration) {
+    Trace::info() << DYNLog(KinRestart) << Trace::endline;
+    restoreState();
+
+    flag = solveStrategy(false, false, KIN_LINESEARCH, multipleStrategiesForAlgebraicRestoration);
+  }
+
   if (flag < 0)
     throw DYNError(Error::SUNDIALS_ERROR, SolverSolveErrorKINSOL);
 
