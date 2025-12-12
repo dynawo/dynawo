8ae71d23cde0dcf2274cbfdfe8bed3289b386b40
Thibaut VERMEULEN <thibaut.vermeulen@rte-france.com>
2025-12-12 08:59:33 +0100

    #3841 fix intermediate dump states in DYNSimulation
    Signed-off-by: Thibaut VERMEULEN <thibaut.vermeulen@rte-france.com>
    
    #3841 add dump action in ZMQ Channel
    
    Signed-off-by: Thibaut VERMEULEN <thibaut.vermeulen@rte-france.com>
    
    #3841 cleaning
    
    Signed-off-by: Thibaut VERMEULEN <thibaut.vermeulen@rte-france.com>
    
    #3841 consider review comments
    
    Signed-off-by: Thibaut VERMEULEN <thibaut.vermeulen@rte-france.com>
    
    #3841 trigger understood as step trigger within clock context
    
    Signed-off-by: Thibaut VERMEULEN <thibaut.vermeulen@rte-france.com>

--- examples/RT/rt-testing-tools/rt-testing-tools/main_zmq_client.py
+++ examples/RT/rt-testing-tools/rt-testing-tools/main_zmq_client.py
@@ -11,20 +11,53 @@
 
 import zmq
 
-class ActionSender:
+class ControlSender:
     def __init__(self, address="tcp://localhost:5555"):
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REQ)
         self.socket.connect(address)
 
-    def send(self, message):
-        print(f"Sending action: {message}")
-        self.socket.send_string(message)
+    def _send(self, topic, format="string", *chunks):
+        print(f"Sending control '{topic}'")
+        if len(chunks) == 0:
+            self.socket.send_string(f"{topic}")
+        else:
+            self.socket.send_string(f"{topic}", zmq.SNDMORE)
+            for chunk in chunks[:-1]:
+                if format == "string":
+                    self.socket.send_string(f"{chunk}", zmq.SNDMORE)
+                elif format == "bytes":
+                    self.socket.send(memoryview(chunk), zmq.SNDMORE)
 
+            # last part:
+            if format == "string":
+                self.socket.send_string(f"{chunks[-1]}")
+            elif format == "bytes":
+                self.socket.send(memoryview(chunks[-1]))
+
+            if topic != "load":
+                print(f'{chunks[-1]}')
         # Wait for acknowledgment
         message = self.socket.recv_string()
-        print(f"return received: {message}")
+        print(f"return received: '{message}'")
 
+    def send_action(self, actions):
+        self._send("action", "string", *actions)
+
+    def send_step_trigger(self):
+        self._send("step")
+
+    def send_stop(self):
+        self._send("stop")
+
+    def send_dump_trigger(self):
+        self._send("dump")
+
+    def send_load_file(self, filename):
+        with open(filename, 'rb') as f:
+            data = f.read()
+            print(f"file size: {len(data)}")
+            self._send("load", "bytes", data)
 
 if __name__ == "__main__":
-    sender = ActionSender()
+    sender = ControlSender()
