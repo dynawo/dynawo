8a1996d0df01e58278e8e2f6c19fa91ccb3e5f50
Florentine Rosiere <florentine.rosiere@rte-france.com>
2026-02-02 19:00:18 +0100

    #3983 disable constraint during init in DYNAFLOW mode to avoid collecting constraints related to badly initialized values closes #3983
    Signed-off-by: Florentine Rosiere <florentine.rosiere@rte-france.com>

--- dynawo/sources/Simulation/DYNSimulation.cpp
+++ dynawo/sources/Simulation/DYNSimulation.cpp
@@ -782,7 +782,10 @@ Simulation::initFromData(const shared_ptr<DataInterface>& data, const shared_ptr
   model_ = modeler.getModel();
   model_->setWorkingDirectory(context_->getWorkingDirectory());
   model_->setTimeline(timeline_);
-  model_->setConstraints(constraintsCollection_);
+
+  if (jobEntry_->getOutputsEntry() && jobEntry_->getOutputsEntry()->getConstraintsEntry() &&
+          jobEntry_->getOutputsEntry()->getConstraintsEntry()->getFilterType() != CONSTRAINTS_DYNAFLOW)
+      model_->setConstraints(constraintsCollection_);
 
   if (jobEntry_->getLocalInitEntry() != nullptr) {
     const std::string initParFile = createAbsolutePath(jobEntry_->getLocalInitEntry()->getParFile(), context_->getInputDirectory());
@@ -907,6 +910,10 @@ Simulation::init() {
   Trace::info() << "-----------------------------------------------------------------------" << Trace::endline<< Trace::endline;
 
   solver_->setTimeline(timeline_);
+  // no constraint registered during initialization
+  if (jobEntry_->getOutputsEntry() && jobEntry_->getOutputsEntry()->getConstraintsEntry() &&
+        jobEntry_->getOutputsEntry()->getConstraintsEntry()->getFilterType() == CONSTRAINTS_DYNAFLOW)
+      model_->setConstraints(constraintsCollection_);
 }
 
 void
