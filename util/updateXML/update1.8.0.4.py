803d7f5df0d46330533bef7eebd2dc3876e3ea9b
Erwan Guichard <erwan.guichard_externe@rte-france.com>
2026-08-14 10:01:44 +0200

    #4188 Scripts renumbered : missing numbers removed
    Signed-off-by: Erwan Guichard <erwan.guichard_externe@rte-france.com>

--- util/updateXML/update1.8.0.4.py
+++ util/updateXML/update1.8.0.4.py
@@ -13,8 +13,8 @@
 
 from content.Ticket import ticket
 
-# update 3 unit dynamic model names in modelica models and model templates
-@ticket(3593)
+# update unit dynamic model names in modelica models and model templates
+@ticket(3443)
 def update(jobs):
     udms = list()
     for mm in jobs.dyds.get_modelica_models(lambda _: True):
@@ -24,12 +24,68 @@ def update(jobs):
         udms.extend(mt.get_unit_dynamic_models(lambda _: True))
 
     for udm in udms:
-        update_name(udm,"Dynawo.Electrical.Wind.WECC","WTG4ACurrentSource","WTG4ACurrentSource1")
-        update_name(udm,"Dynawo.Electrical.Controls.WECC.Mechanical","DriveTrainPmConstant","WTGTa")
-        update_name(udm,"Dynawo.Electrical.Controls.WECC.Mechanical","DriveTrainPeFiltered","WTGTb")
+        update_name(udm,"Dynawo.Electrical.BESS.WECC","BESScbCurrentSource","BESSCurrentSource")
+        update_name(udm,"Dynawo.Electrical.BESS.WECC","BESScbCurrentSourceNoPlantControl","BESSCurrentSourceNoPlantControl")
+        update_name(udm,"Dynawo.Electrical.Photovoltaics.WECC","PVCurrentSourceB","PVCurrentSource")
+        update_name(udm,"Dynawo.Electrical.Photovoltaics.WECC","PVCurrentSourceBNoPlantControl","PVCurrentSourceNoPlantControl")
+        update_name(udm,"Dynawo.Electrical.Photovoltaics.WECC","PVVoltageSourceA","PVVoltageSource1")
+        update_name(udm,"Dynawo.Electrical.Photovoltaics.WECC","PVVoltageSourceANoPlantControl","PVVoltageSource1NoPlantcontrol")
+        update_name(udm,"Dynawo.Electrical.Photovoltaics.WECC","PVVoltageSourceB","PVVoltageSource2")
+        update_name(udm,"Dynawo.Electrical.Photovoltaics.WECC","PVVoltageSourceBNoPlantControl","PVVoltageSource2NoPlantControl")
 
-    for windxml in jobs.dyds.get_bbms(lambda bbm: bbm.get_lib_name() == "WTG4AWeccCurrentSource"):
-        windxml.set_lib_name("WTG4AWeccCurrentSource1")
+    for bessxml in jobs.dyds.get_bbms(lambda bbm: bbm.get_lib_name() == "BESScbWeccCurrentSource"):
+        bessxml.set_lib_name("BESSWeccCurrentSource")
+    for bessxml in jobs.dyds.get_bbms(lambda bbm: bbm.get_lib_name() == "BESScbWeccCurrentSourceNoPlantControl"):
+        bessxml.set_lib_name("BESSWeccCurrentSourceNoPlantControlSource")
+    for pvxml in jobs.dyds.get_bbms(lambda bbm: bbm.get_lib_name() == "PhotovoltaicsWeccCurrentSourceB"):
+        pvxml.set_lib_name("PhotovoltaicsWeccCurrentSource")
+    for pvxml in jobs.dyds.get_bbms(lambda bbm: bbm.get_lib_name() == "PhotovoltaicsWeccCurrentSourceBNoPlantControl"):
+        pvxml.set_lib_name("PhotovoltaicsWeccCurrentSourceNoPlantControl")
+    for pvxml in jobs.dyds.get_bbms(lambda bbm: bbm.get_lib_name() == "PhotovoltaicsWeccVoltageSourceA"):
+        pvxml.set_lib_name("PhotovoltaicsWeccVoltageSource1")
+    for pvxml in jobs.dyds.get_bbms(lambda bbm: bbm.get_lib_name() == "PhotovoltaicsWeccVoltageSourceANoPlantControl"):
+        pvxml.set_lib_name("PhotovoltaicsWeccVoltageSource1NoPlantControl")
+    for pvxml in jobs.dyds.get_bbms(lambda bbm: bbm.get_lib_name() == "PhotovoltaicsWeccVoltageSourceB"):
+        pvxml.set_lib_name("PhotovoltaicsWeccVoltageSource2")
+    for pvxml in jobs.dyds.get_bbms(lambda bbm: bbm.get_lib_name() == "PhotovoltaicsWeccVoltageSourceBNoPlantControl"):
+        pvxml.set_lib_name("PhotovoltaicsWeccVoltageSource2NoPlantControl")
+
+    for lib_name in ["WTG3WeccCurrentSource1","WTG3WeccCurrentSource2"]:
+        for wtg3 in jobs.dyds.get_bbms(lambda bbm: lib_name in bbm.get_lib_name()):
+            wtg3.parset.add_param("BOOL", "WTG3_Lvplsw", False)
+            wtg3.parset.add_param("DOUBLE", "WTG3_zerox", 0.1)
+            wtg3.parset.add_param("DOUBLE", "WTG3_brkpt", 0.05)
+            wtg3.parset.add_param("DOUBLE", "WTG3_lvpl1", 1.22)
+
+    for wtg4b in jobs.dyds.get_bbms(lambda bbm: "WTG4BWeccCurrentSource" in bbm.get_lib_name()):
+        wtg4b.parset.add_param("BOOL", "WTG4B_Lvplsw", False)
+        wtg4b.parset.add_param("DOUBLE", "WTG4B_zerox", 0.1)
+        wtg4b.parset.add_param("DOUBLE", "WTG4B_brkpt", 0.05)
+        wtg4b.parset.add_param("DOUBLE", "WTG4B_lvpl1", 1.22)
+
+    for wtg4a in jobs.dyds.get_bbms(lambda bbm: "WTG4AWeccCurrentSource" in bbm.get_lib_name()):
+        wtg4a.parset.add_param("BOOL", "WTG4A_Lvplsw", False)
+        wtg4a.parset.add_param("DOUBLE", "WTG4A_zerox", 0.1)
+        wtg4a.parset.add_param("DOUBLE", "WTG4A_brkpt", 0.05)
+        wtg4a.parset.add_param("DOUBLE", "WTG4A_lvpl1", 1.22)
+
+    for wt4b in jobs.dyds.get_bbms(lambda bbm: "WT4BWeccCurrentSource" in bbm.get_lib_name()):
+        wt4b.parset.add_param("BOOL", "WT4B_Lvplsw", False)
+        wt4b.parset.add_param("DOUBLE", "WT4B_zerox", 0.1)
+        wt4b.parset.add_param("DOUBLE", "WT4B_brkpt", 0.05)
+        wt4b.parset.add_param("DOUBLE", "WT4B_lvpl1", 1.22)
+
+    for wt4a in jobs.dyds.get_bbms(lambda bbm: "WT4AWeccCurrentSource" in bbm.get_lib_name()):
+        wt4a.parset.add_param("BOOL", "WT4A_Lvplsw", False)
+        wt4a.parset.add_param("DOUBLE", "WT4A_zerox", 0.1)
+        wt4a.parset.add_param("DOUBLE", "WT4A_brkpt", 0.05)
+        wt4a.parset.add_param("DOUBLE", "WT4A_lvpl1", 1.22)
+
+    for bess in jobs.dyds.get_bbms(lambda bbm: "BESSWeccCurrentSource" in bbm.get_lib_name()):
+        bess.parset.add_param("BOOL", "BESS_Lvplsw", False)
+        bess.parset.add_param("DOUBLE", "BESS_zerox", 0.1)
+        bess.parset.add_param("DOUBLE", "BESS_brkpt", 0.05)
+        bess.parset.add_param("DOUBLE", "BESS_lvpl1", 1.22)
 
 def update_name(udm,prefix,old,new):
     if udm.get_name() == prefix+"."+old:
--- util/updateXML/update1.8.0.8.py
+++ util/updateXML/update1.8.0.8.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2025, RTE (http://www.rte-france.com)
+# Copyright (c) 2026, RTE (http://www.rte-france.com)
 # See AUTHORS.txt
 # All rights reserved.
 # This Source Code Form is subject to the terms of the Mozilla Public
@@ -13,16 +13,33 @@
 
 from content.Ticket import ticket
 
-# GeneratorFictitious -> GeneratorAlphaBeta
-@ticket(3897)
+# Remove outdated WPP parameters, add new ones, replace a single INIT model by two new ones
+@ticket(3612)
 def update(jobs):
+    wpps = jobs.dyds.get_bbms(lambda bbm: "IECWPP" in bbm.get_lib_name())
+    for wpp in wpps:
+        wpp.parset.remove_param_or_ref("BesPu")
+        wpp.parset.remove_param_or_ref("GesPu")
+        wpp.parset.remove_param_or_ref("ResPu")
+        wpp.parset.remove_param_or_ref("XesPu")
+        wpp.parset.add_param("BOOL", "WPP_PPCLocal", True)
+        wpp.parset.add_param("DOUBLE", "WPP_BMvHvPu", 0)
+        wpp.parset.add_param("DOUBLE", "WPP_GMvHvPu", 0)
+        wpp.parset.add_param("DOUBLE", "WPP_RMvHvPu", 0)
+        wpp.parset.add_param("DOUBLE", "WPP_XMvHvPu", 0)
+        wpp.parset.add_param("BOOL", "WPP_ConverterLVControl", False)
+        wpp.parset.add_param("DOUBLE", "WPP_BLvTrPu", 0)
+        wpp.parset.add_param("DOUBLE", "WPP_GLvTrPu", 0)
+        wpp.parset.add_param("DOUBLE", "WPP_RLvTrPu", 0)
+        wpp.parset.add_param("DOUBLE", "WPP_XLvTrPu", 0)
+
     modelica_models = jobs.dyds.get_modelica_models(lambda _: True)
     for modelica_model in modelica_models:
         unit_dynamic_models = modelica_model.get_unit_dynamic_models(
-            lambda unit_dynamic_model: unit_dynamic_model.get_name() == "Dynawo.Electrical.Machines.Simplified.GeneratorFictitious"
-        )
+            lambda unit_dynamic_model: "CurrentSource2015" in unit_dynamic_model.get_name())
         for unit_dynamic_model in unit_dynamic_models:
-            unit_dynamic_model.set_name("Dynawo.Electrical.Machines.Simplified.GeneratorAlphaBeta")
-
-    for genxml in jobs.dyds.get_bbms(lambda bbm: bbm.get_lib_name() == "GeneratorFictitious"):
-        genxml.set_lib_name("GeneratorAlphaBeta")
+            unit_dynamic_model.set_init_name("Dynawo.Electrical.Wind.IEC.WPP.WPP4CurrentSource2015_INIT")
+        unit_dynamic_models = modelica_model.get_unit_dynamic_models(
+            lambda unit_dynamic_model: "CurrentSource2020" in unit_dynamic_model.get_name())
+        for unit_dynamic_model in unit_dynamic_models:
+            unit_dynamic_model.set_init_name("Dynawo.Electrical.Wind.IEC.WPP.WPP4CurrentSource2020_INIT")
